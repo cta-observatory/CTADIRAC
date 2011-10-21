82e34d1a72ae4961f87875b1d338b0368f06cf91
Luisa Arrabito <arrabito@in2p3.fr>
2011-10-21 14:05:47 +0000

    cta-hap-application

---
+++ Core/scripts/cta-hap-application.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import DIRAC
+
+def main():
+
+  from DIRAC.Core.Base import Script
+  Script.parseCommandLine()
+  from HapApplication import HapApplication
+
+  DIRAC.gLogger.notice( 'Executing a Hap Application' )
+
+  args = Script.getPositionalArgs()
+  DIRAC.gLogger.notice( 'Arguments:', args )
+
+  ha = HapApplication()
+  ha.setSoftwarePackage('HAP/v0.1/HAP')
+  # There is a bug in the Job.py class that produce a duplicated is the first argument
+  if args[1].find( args[0] ) == 0:
+#    rm.rootMacro = args[1]
+    ha.rootArguments = args[1:]
+  else:
+#    rm.rootMacro = args[0]
+    ha.rootArguments = args[0:]
+
+  res = ha.execute()
+
+  if not res['OK']:
+    DIRAC.exit( -1 )
+
+  DIRAC.exit()
+
+if __name__ == '__main__':
+
+  try:
+    main()
+  except Exception:
+    DIRAC.gLogger.exception()
+    DIRAC.exit( -1 )
