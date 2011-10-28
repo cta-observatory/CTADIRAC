1f7307bc36899190cb8c6735fc906634ffb1c0a8
Luisa Arrabito <arrabito@in2p3.fr>
2011-10-28 14:35:23 +0000

    Update

--- Core/scripts/cta-hap-application.py
+++ Core/scripts/cta-hap-application.py
@@ -4,21 +4,43 @@ import DIRAC
 def main():
 
   from DIRAC.Core.Base import Script
-  Script.parseCommandLine()
-  from CTADIRAC.Core.Workflow.Modules.HapApplication import HapApplication
+  Script.registerSwitch( "infile", "infile", "Input file" )
+  Script.registerSwitch( "outfile", "outfile", "Output file" )
+  Script.registerSwitch( "tellist", "tellist", "Configuration file" )
 
+  Script.parseCommandLine( ignoreErrors = True )
+#  Script.parseCommandLine()
+
+  from CTADIRAC.Core.Workflow.Modules.HapApplication import HapApplication
+  from CTADIRAC.Core.Utilities.SoftwareInstallation import localArea
+  import os
+   
   DIRAC.gLogger.notice( 'Executing a Hap Application' )
 
   args = Script.getPositionalArgs()
   DIRAC.gLogger.notice( 'Arguments:', args )
 
   ha = HapApplication()
-  ha.setSoftwarePackage('HAP/v0.1/HAP')
+  ha.setSoftwarePackage('HAP/v0.1/HAP') 
+
+
+
+#  ha.setSoftwarePackage() 
   # There is a bug in the Job.py class that produce a duplicated is the first argument
-  if args[1].find( args[0] ) == 0:
-    ha.hapArguments = args[1:]
-  else:
-    ha.hapArguments = args[0:]
+
+  if args[0] == 'jobDescription.xml' or args[0] == 'eventio_cta':
+    args=args[1:]
+
+
+  args[2] = os.path.join( localArea(),
+                       'HAP/v0.1/config/%s' % args[2])
+
+  args[0] = '-file ' + args[0]
+  args[1] = '-o ' + args[1]
+  args[2] = '-tellist ' + args[2] 
+
+
+  ha.hapArguments = args
 
   res = ha.execute()
 
