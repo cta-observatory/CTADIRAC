c6060e59faf72b4534f8dbe760f69fe2dac7f77a
Luisa Arrabito <arrabito@in2p3.fr>
2012-01-31 13:21:33 +0000

    Clone of the HapJob class

---
+++ Interfaces/API/HapConverterJob.py
@@ -0,0 +1,46 @@
+"""
+  Simple Wrapper on the Job class to handle HAP converter
+"""
+
+__RCSID__ = "$Id$"
+
+from DIRAC.Interfaces.API.Job import Job
+from DIRAC.Core.Workflow.Workflow                   import Workflow
+#####################
+from CTADIRAC.Core.Utilities import SoftwareInstallation
+import os
+
+class HapConverterJob( Job ) :
+
+
+  def __init__( self, script,  parameters = None, cpuTime = 3600 ):
+
+    Job.__init__( self )
+
+    self.workflow = Workflow()
+    self.executable = '$DIRACROOT/scripts/cta-hap-converter'
+    self.setName( os.path.basename( script ) )
+    self.setCPUTime( cpuTime )
+    global argumentStr
+    argumentStr = "%s" % ( ' '.join( parameters ) )
+    self.setConfigArgs( argumentStr )
+
+  def setVersion(self, version):
+    versionStr = ' ' + '-V' + ' ' + version
+    global argumentStr
+    argumentStr= argumentStr + versionStr
+    self.setConfigArgs( argumentStr )
+    
+  def setConfig(self, config):
+    configStr = ' ' + '-T' + ' ' + config
+    global argumentStr
+    argumentStr= argumentStr + configStr
+    self.setConfigArgs( argumentStr )
+
+
+
+
+
+
+
+
