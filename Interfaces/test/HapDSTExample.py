e879f3626efb65127165b8ba99692e41190b948a
Luisa Arrabito <arrabito@in2p3.fr>
2012-02-16 12:15:52 +0000

    New python script to launch a HapDST job

---
+++ Interfaces/test/HapDSTExample.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""
+  Submit an Example HapDST Job
+"""
+from DIRAC.Core.Base import Script
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s [option|cfgfile] ... [Site] ...' % Script.scriptName,
+                                     'Arguments:',
+                                     '  Site:     Requested Site' ] ) )
+Script.parseCommandLine()
+
+import os
+
+def HapDSTExample( destination = None ) :
+  from CTADIRAC.Interfaces.API.HapDSTJob import HapDSTJob
+  from DIRAC.Interfaces.API.Dirac import Dirac
+
+  infileLFN = 'LFN:/vo.cta.in2p3.fr/HAPtest/rawdata/proton_20deg_90deg_run89580___cta-ultra3_desert.array-E.root'
+
+  RunNum = 1002
+  FileName = os.path.basename(infileLFN)
+  ArrayConfig = 'array-E.lis'
+  Nevent=100000
+  fileout = '/tmp/dst_CTA_0000' + str(RunNum) + '.root'
+  toCompile = False 
+   
+  j = RootJob( 'make_CTA_DST.C', [RunNum,FileName,ArrayConfig,Nevent], toCompile )
+
+  if destination:
+    j.setDestination( destination )
+
+  j.setInputSandbox( [ 'passphrase' ] )
+
+  j.setInputData([infileLFN])
+  j.setOutputData(fileout)
+  j.setCPUTime(200000)
+  j.setPlatform( "gLite-HighMem" )
+
+  Script.gLogger.info( j._toJDL() )
+
+  return Dirac().submit( j )
+
+
+
+if __name__ == '__main__':
+
+  args = Script.getPositionalArgs()
+
+  ret = RootExample( args )
+  if ret['OK']:
+    Script.gLogger.notice( 'Submitted Job:', ret['Value'] )
+  else:
+    Script.gLogger.error( ret['Message'] )
