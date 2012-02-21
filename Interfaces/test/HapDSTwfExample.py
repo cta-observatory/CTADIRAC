4b7d994620ea6bf781021dc7f0fcb284359b2c23
Luisa Arrabito <arrabito@in2p3.fr>
2012-02-21 12:47:37 +0000

    Script to run a HAP DST wf

---
+++ Interfaces/test/HapDSTwfExample.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+"""
+  Submit an Example Hap Converter Job
+"""
+from DIRAC.Core.Base import Script
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s [option|cfgfile] ... [Site] ...' % Script.scriptName,
+                                     'Arguments:',
+                                     '  Site:     Requested Site' ] ) )
+Script.parseCommandLine()
+
+import os
+
+def HapDSTwfExample( destination = None ) :
+  from CTADIRAC.Interfaces.API.HapDSTwfJob import HapDSTwfJob
+  from DIRAC.Interfaces.API.Dirac import Dirac
+
+  HapVersion = 'v0.3'
+  config = 'array-E.lis'
+  infileLFN = 'LFN:/vo.cta.in2p3.fr/Simu2/v_Leeds/Data/sim_hessarray/cta-ultra3/0.0deg/Data/proton_20deg_90deg_run89580___cta-ultra3_desert.simhess.gz'
+  infile = os.path.basename(infileLFN)
+  fileout = infile.replace('simhess.gz',os.path.splitext(os.path.basename(config))[0] + '.root')
+  Nevents = '100000' ####### Nevents for the make_CTA_DST.C macro
+   
+  j = HapConverterJob('eventio_cta',['-I',infile,'-O', fileout,'-N',Nevents])   
+
+  j.setVersion(HapVersion)
+  j.setConfig(config)
+
+  if destination:
+    j.setDestination( destination )
+
+  j.setInputSandbox( [ 'passphrase','cta-hap-wf','/home/arrabito/sw/pro/CTADIRAC'] )
+  j.setName('HapDSTwf')
+  j.setInputData([infileLFN])
+  j.setOutputData(fileout)
+### add dst output #####################
+  j.setPlatform( "gLite-HighMem" )
+
+  Script.gLogger.info( j._toJDL() )
+
+  return Dirac().submit( j )
+
+if __name__ == '__main__':
+
+  args = Script.getPositionalArgs()
+
+  ret = HapDSTwfExample( args )
+
+  if ret['OK']:
+    Script.gLogger.notice( 'Submitted Job:', ret['Value'] )
+  else:
+    Script.gLogger.error( ret['Message'] )
+
