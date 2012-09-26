9dd194088d87dbb01f4359b2b0979011bb18c3fc
Luisa Arrabito <arrabito@in2p3.fr>
2012-09-26 13:50:08 +0000

    Bug fix: HapConverterJob-> HapConverterParamJob

--- Interfaces/test/HapConverterParamExample.py
+++ Interfaces/test/HapConverterParamExample.py
@@ -13,7 +13,7 @@ Script.parseCommandLine()
 import os
 
 def HapConverterParamExample( destination = None ) :
-  from CTADIRAC.Interfaces.API.HapConverterJob import HapConverterJob
+  from CTADIRAC.Interfaces.API.HapConverterParamJob import HapConverterParamJob
   from DIRAC.Interfaces.API.Dirac import Dirac
 
 ### general options ###############
@@ -23,7 +23,6 @@ def HapConverterParamExample( destination = None ) :
   'LFN:/vo.cta.in2p3.fr/Simulation/sim_telarray/Prod1S_PS/2000/gamma/20/90/spectrum_-2.0/0.003_300/pointlike/cta-prod1/0.0deg/Data/run283xxx/gamma_20deg_90deg_run283000___cta-prod1_desert.simhess.gz',
   'LFN:/vo.cta.in2p3.fr/Simulation/sim_telarray/Prod1S_PS/2000/gamma/20/90/spectrum_-2.0/0.003_300/pointlike/cta-prod1/0.0deg/Data/run283xxx/gamma_20deg_90deg_run283001___cta-prod1_desert.simhess.gz']
 
-
   fileout_prefix = 'raw_' 
   tellist = 'array-E.lis'
 
@@ -32,18 +31,16 @@ def HapConverterParamExample( destination = None ) :
 
   opts =  general_opts + eventio_cta_opts
 
-  j = HapConverterJob(opts)
+  j = HapConverterParamJob(opts)
 
+  j.setParametricInputData(infileLFNList)  
+  j.setInputSandbox( [ 'passphrase','check_raw.csh'] )  
+  j.setOutputSandbox( ['eventio_cta.log','Open_Raw.log'])
+  j.setOutputData(['raw_*.root'])
+  j.setName('eventio')
+  
   if destination:
     j.setDestination( destination )
-
-  j.setInputSandbox( [ 'passphrase','check_raw.csh','CTADIRAC','cta-hap-converter.py'] )
-  j.setOutputSandbox( ['eventio_cta.log','Open_Raw.log'])
-  j.setParametricInputData(infileLFNList)
-
-  j.setName('eventio')
-
-  j.setOutputData(['raw_*.root'])
 
   j.setCPUTime(100000)
   Script.gLogger.info( j._toJDL() )
