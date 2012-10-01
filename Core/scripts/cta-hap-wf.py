f912df0902861b74d57a77cfe8a39a56ad080fe6
Luisa Arrabito <arrabito@in2p3.fr>
2012-10-01 10:26:31 +0000

    Bug fix: indentation error.

--- Core/scripts/cta-hap-wf.py
+++ Core/scripts/cta-hap-wf.py
@@ -199,7 +199,8 @@ def main():
     error = 'make_CTA_DST.C: DST file not found'
     DIRAC.gLogger.error(error)
     jobReport.setApplicationStatus(error)
-	  DIRAC.exit(-1)
+    DIRAC.exit(-1)
+ 
       
 ################################################
   DIRAC.gLogger.notice('Executing DST Check step0')
@@ -214,13 +215,13 @@ def main():
 
   status, stdout, stderr = ret['Value']
   if status==1:
-    jobReport.setApplicationStatus('Check_dst0: Big problem during the DST production')
-	  DIRAC.gLogger.error( 'DST Check step0 reports: Big problem during the DST production' )
-	  DIRAC.exit( -1 )
+    jobReport.setApplicationStatus('Check_dst0: Big problem during the DST production')  
+    DIRAC.gLogger.error( 'DST Check step0 reports: Big problem during the DST production' )
+    DIRAC.exit(-1)
   if status==2:
     jobReport.setApplicationStatus('Check_dst0: No triggered events')
-	  DIRAC.gLogger.notice( 'DST Check step0 reports: No triggered events' )
-	  DIRAC.exit()
+    DIRAC.gLogger.notice( 'DST Check step0 reports: No triggered events' )
+    DIRAC.exit()
 
 ############# run CheckDST.C #################
   DIRAC.gLogger.notice('Executing DST check step1')
@@ -251,8 +252,8 @@ def main():
   status, stdout, stderr = ret['Value']
   if status==1:
     jobReport.setApplicationStatus('DST Check step2: Big problem during the DST production')
-	  DIRAC.gLogger.error( 'DST Check step2 reports: Big problem during the DST production' )
-	  DIRAC.exit( -1 )
+     DIRAC.gLogger.error( 'DST Check step2 reports: Big problem during the DST production' )
+    DIRAC.exit( -1 )     
   if status==2:
     jobReport.setApplicationStatus('DST Check step2: No triggered events')
 	  DIRAC.gLogger.notice( 'DST Check step2 reports: No triggered events' )
