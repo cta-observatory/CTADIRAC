2b94497fbdfcff9e2c30c58a88d33675481345ec
Luisa Arrabito <arrabito@in2p3.fr>
2012-12-26 14:04:59 +0000

    update for prod2.

--- Interfaces/test/CorsikaSimtelProdExample.py
+++ Interfaces/test/CorsikaSimtelProdExample.py
@@ -1,54 +1,55 @@
 #!/usr/bin/env python
 """
-  Submit an Example Hap Converter Job
+  Submit a Corsika Example Job
 """
 from DIRAC.Core.Base import Script
 Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                      'Usage:',
-                                     '  %s [option|cfgfile] ... [Site] ...' % Script.scriptName,
+                                     '  %s [option|cfgfile] ... [runMin] ...' % Script.scriptName,
                                      'Arguments:',
-                                     '  Site:     Requested Site' ] ) )
+                                     '  runMin:     Min runNumber',
+                                     '  runMax:     Max runNumber' ] ) )
 Script.parseCommandLine()
 
 import os
 
-def CorsikaSimtelProdExample( destination = None ) :
+def CorsikaSimtelExample( args = None ) :
+  
   from CTADIRAC.Interfaces.API.CorsikaSimtelProdJob import CorsikaSimtelProdJob
   from DIRAC.Interfaces.API.Dirac import Dirac
-
   
   j = CorsikaSimtelProdJob()
-  j.setVersion('clean_23012012')
+  j.setVersion('prod-2_21122012')
 
-  executable = 'corsika_autoinputs'
-  j.setExecutable(executable) 
+  j.setExecutable('corsika_autoinputs') 
+
+  mode = 'corsika_simtel'
+#  mode = 'corsika_standalone'
+  storage_element = 'CC-IN2P3-Disk'
 
   ilist = []
-  for i in range(1,5):
+
+  if (len(args) != 2):
+    Script.showHelp()
+
+  runMin = int(args[0])
+  runMax = int(args[1])
+
+  for i in range(runMin,runMax+1):
     run_number = '%06d' % i
     ilist.append(run_number)
 
-  j.setGenericParametricInput(ilist)
-#######################################                                                                                           
+  j.setGenericParametricInput(ilist)                                                                                         
   j.setName('run%s')
+  
+  j.setInputSandbox( [ 'INPUTS_CTA_PROD2_proton_South','fileCatalog.cfg','prodConfigFile'] )
 
-  j.setParameters(['fileCatalog.cfg','--run','corsika','--template','INPUTS_CTA_ULTRA3_proton'])
+  j.setParameters(['fileCatalog.cfg','--template','INPUTS_CTA_PROD2_proton_South','--mode',mode,'-D',storage_element])
 
-  if destination:
-    j.setDestination( destination )
-
-  j.setInputSandbox( [ 'INPUTS_CTA_ULTRA3_proton','fileCatalog.cfg','prodConfigFile'] )
-  outlog = executable + '.log'
-  j.setOutputSandbox( [outlog])
-
-  sim_out = 'Data/sim_telarray/cta-ultra3/0.0deg/Data/*.simtel.gz'
-  log_out = 'Data/sim_telarray/cta-ultra3/0.0deg/Log/*.log.gz'
-  hist_out = 'Data/sim_telarray/cta-ultra3/0.0deg/Histograms/*.hdata.gz'
-  corsika_out = 'run%s.corsika.gz'
-  corsikatar_out = 'corsika_run%s.tar.gz'
-
+  j.setOutputSandbox( ['corsika_autoinputs.log', 'simtel.log'])
 
   j.setCPUTime(100000)
+
   Script.gLogger.info( j._toJDL() )
   Dirac().submit( j )
 
@@ -56,7 +57,8 @@ def CorsikaSimtelProdExample( destination = None ) :
 if __name__ == '__main__':
 
   args = Script.getPositionalArgs()
-  print "args:",args
-  CorsikaSimtelProdExample( args )
 
-
+  try:
+    CorsikaSimtelProdExample( args )
+  except Exception:
+    Script.gLogger.exception()
