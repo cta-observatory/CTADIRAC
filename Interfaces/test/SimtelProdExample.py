902071fbe4ded7eccd9bb1d0917889beb017cf1e
Luisa Arrabito <arrabito@in2p3.fr>
2013-05-06 08:31:48 +0000

    Introduction of CODE in the ISB.

--- Interfaces/test/SimtelProdExample.py
+++ Interfaces/test/SimtelProdExample.py
@@ -8,7 +8,7 @@ Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                      '  %s [option|cfgfile] ... [inputfilelist] ...' % Script.scriptName,
                                      'Arguments:',
                                      '  inputfilelist: Input File List',
-                                     '  simtelConfig: cta-ultra5/4MSST',
+                                     '  simtelConfig: cta-ultra5/4MSST/SCMST',
                                      '  storageElement: Storage Element'] ) )
 Script.parseCommandLine()
 
@@ -19,13 +19,14 @@ def SimtelProdExample( args = None ) :
   from CTADIRAC.Interfaces.API.SimtelProdJob import SimtelProdJob
   from DIRAC.Interfaces.API.Dirac import Dirac
 
-  if (len(args)<1 or len(args)>2):
+  if (len(args)<1 or len(args)>3):
     Script.showHelp()
 
   if (len(args)==3):
     storage_element = args[2]
   else :
     storage_element = 'LUPM-Disk'
+
 
   LFN_file = args[0]
   f = open(LFN_file,'r')
@@ -39,15 +40,26 @@ def SimtelProdExample( args = None ) :
  
   j.setVersion('prod-2_08032013')
 
-  j.setParametricInputData(infileLFNList) 
+  j.setParametricInputData(infileLFNList)
 
-  j.setExecutable('prod2_qgs2') 
+#  j.setExecutable('prod2_qgs2')
+  j.setExecutable('sc2_111_qgs2')
   
-  simtel_config = args[1]
+  if args[1] in ['cta-ultra5','4MSST','SCMST']:
+    simtel_config = args[1]
+  else:
+    Script.showHelp()
 
   j.setParameters(['fileCatalog.cfg','-S',simtel_config,'-D',storage_element])
 
-  j.setInputSandbox( [ 'fileCatalog.cfg','CONFIG'] )
+  config_dict = {'4MSST':'4mDC','SCMST':'SC-MST'}
+ 
+  if simtel_config in ['4MSST','SCMST']:
+    if not os.path.isdir(os.path.join('CONFIG',config_dict[simtel_config])):
+      Script.gLogger.error('Missing Config directory')
+      Script.showHelp()
+      
+  j.setInputSandbox( [ 'fileCatalog.cfg','CODE','CONFIG'] )
 
   j.setOutputSandbox( ['simtel.log'])
 
@@ -59,8 +71,10 @@ def SimtelProdExample( args = None ) :
   j.setCPUTime(100000)
 
   Script.gLogger.info( j._toJDL() )
-  Dirac().submit( j )
 
+  jobID = Dirac().submit( j )
+
+  print 'Submission Result: ',jobID
 
 if __name__ == '__main__':
 
