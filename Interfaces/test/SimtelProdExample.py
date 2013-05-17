93e0b77b0d8a0302855799dd07ac691ffea89962
Luisa Arrabito <arrabito@in2p3.fr>
2013-05-17 08:20:36 +0000

    Allow to run individual or 6 config at a time.

--- Interfaces/test/SimtelProdExample.py
+++ Interfaces/test/SimtelProdExample.py
@@ -8,8 +8,8 @@ Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                      '  %s [option|cfgfile] ... [inputfilelist] ...' % Script.scriptName,
                                      'Arguments:',
                                      '  inputfilelist: Input File List',
-                                     '  simtelConfig: cta-ultra5/4MSST/SCMST',
-                                     '  storageElement: Storage Element'] ) )
+                                     '  storageElement: Storage Element',
+                                     ' reprocessing configuration (STD,NSBX3,SCMST,SCSST,4MSST,ASTRI,6INROW)'] ) )
 Script.parseCommandLine()
 
 import os
@@ -19,14 +19,10 @@ def SimtelProdExample( args = None ) :
   from CTADIRAC.Interfaces.API.SimtelProdJob import SimtelProdJob
   from DIRAC.Interfaces.API.Dirac import Dirac
 
-  if (len(args)<1 or len(args)>3):
-    Script.showHelp()
-
-  if (len(args)==3):
-    storage_element = args[2]
-  else :
+  if (len(args)>=2):
+    storage_element = args[1]
+  else:
     storage_element = 'LUPM-Disk'
-
 
   LFN_file = args[0]
   f = open(LFN_file,'r')
@@ -34,51 +30,46 @@ def SimtelProdExample( args = None ) :
   infileLFNList = []
   for line in f:
     infileLFN = line.strip()
-    infileLFNList.append(infileLFN)
+    if line!="\n":
+      infileLFNList.append(infileLFN)
 
   j = SimtelProdJob()
- 
-  j.setVersion('prod-2_08032013')
 
-  j.setParametricInputData(infileLFNList) 
+  j.setVersion('prod-2_06052013')
+  #cfg = 'cta-prod2' #this is hardcoded in generic_run
+  #simtelArrayConfig, passed as argument to this script or defaulted to STD, refers to the MD in the catalog and the actual storage path.
+  simtelArrayConfig = "STD"
 
-#  j.setExecutable('prod2_qgs2') 
-  j.setExecutable('sc2_111_qgs2') 
-  
-  if args[1] in ['cta-ultra5','4MSST','SCMST']:
-    simtel_config = args[1]
-  else:
-    Script.showHelp()
-
-  j.setParameters(['fileCatalog.cfg','-S',simtel_config,'-D',storage_element])
-
-  config_dict = {'4MSST':'4mDC','SCMST':'SC-MST'}
- 
-  if simtel_config in ['4MSST','SCMST']:
-    if not os.path.isdir(os.path.join('CONFIG',config_dict[simtel_config])):
-      Script.gLogger.error('Missing Config directory') 
+  if (len(args)>=3):
+    if args[2] not in ['STD','NSBX3','SCMST','SCSST','4MSST','ASTRI','6INROW']:
+      print "arrayConfig argument %s incorrect"%args[2]
       Script.showHelp()
-      
-  j.setInputSandbox( [ 'fileCatalog.cfg','CODE','CONFIG'] )
 
-  j.setOutputSandbox( ['simtel.log'])
+    simtelArrayConfig = args[2]
 
-  name = 'repro_' + simtel_config
+  j.setParametricInputData(infileLFNList)
+  j.setType( 'CorsikaRepro' )
+
+  name = 'repro_' + simtelArrayConfig
   j.setName(name)
- 
   j.setJobGroup(name)
 
+  j.setInputSandbox( [ 'fileCatalog.cfg','CTADIRAC','cta-simtelprodold.py','grid_prod2-repro.sh','LFN:/vo.cta.in2p3.fr/user/j/johann.cohen-tanugi/PROD2/SVN-PROD2_rev1869.tar.gz'] )
+
+  j.setParameters(['fileCatalog.cfg','-D',storage_element,'-S',simtelArrayConfig])
+  j.setOutputSandbox( ['simtel.log'])
   j.setCPUTime(100000)
-
   Script.gLogger.info( j._toJDL() )
+  Dirac().submit( j )
 
-  jobID = Dirac().submit( j )
-
-  print 'Submission Result: ',jobID
 
 if __name__ == '__main__':
 
   args = Script.getPositionalArgs()
+
+  if len(args)!=3:
+    Script.gLogger.notice('need 3 args : 1/file of corsika lfns 2/Storage Element 3/Config')
+    Script.showHelp()
 
   try:
     SimtelProdExample( args )
