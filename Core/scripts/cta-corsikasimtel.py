b3a6f465013a34311f6bd9112cbad45f7cb0159f
Luisa Arrabito <arrabito@in2p3.fr>
2013-11-06 10:15:32 +0000

    Update to be more similar to prod. jobs

--- Core/scripts/cta-corsikasimtel.py
+++ Core/scripts/cta-corsikasimtel.py
@@ -16,10 +16,8 @@ def main():
 
   from DIRAC.Core.Base import Script
 
-#  Script.registerSwitch( "R:", "run=", "Run" )
-#  Script.registerSwitch( "P:", "config_path=", "Config Path" )
   Script.registerSwitch( "T:", "template=", "Corsika Template" )
-  Script.registerSwitch( "S:", "simexe=", "Simtel Executable")
+  Script.registerSwitch( "S:", "simtelConfig=", "SimtelConfig" )
   Script.registerSwitch( "p:", "run_number=", "Do not use: Run Number automatically set" )
   Script.registerSwitch( "E:", "executable=", "Executable (Use SetExecutable)")
   Script.registerSwitch( "V:", "version=", "Version (Use setVersion)")
@@ -30,8 +28,8 @@ def main():
 
   ## default values ##############
   run_number = None
-  template = None
-  simexe = None
+  corsikaTemplate = None
+  simtelConfig = None
   executable = None
   version = None
   mode = 'corsika_simtelarray'
@@ -41,9 +39,9 @@ def main():
     if switch[0] == "run_number" or switch[0] == "p":
       run_number = switch[1].split('ParametricParameters=')[1]
     elif switch[0] == "template" or switch[0] == "T":
-      template = switch[1]
-    elif switch[0] == "simexe" or switch[0] == "S":
-      simexe = switch[1]
+      corsikaTemplate = switch[1]
+    elif switch[0] == "simtelConfig" or switch[0] == "S":
+      simtelConfig = switch[1]
     elif switch[0] == "executable" or switch[0] == "E":
       executable = switch[1]
     elif switch[0] == "version" or switch[0] == "V":
@@ -52,7 +50,7 @@ def main():
       mode = switch[1]
       
   
-  if version == None or executable == None or run_number == None or template == None:
+  if version == None or executable == None or run_number == None or corsikaTemplate == None:
     Script.showHelp()
     jobReport.setApplicationStatus('Missing options')
     DIRAC.exit( -1 )
@@ -71,10 +69,137 @@ def main():
   jobID = int( jobID )
   jobReport = JobReport( jobID )
 
+  createGlobalsFromConfigFiles(corsikaTemplate,version)
+
+  ############ Producing Corsika File
+
+  install_CorsikaSimtelPack(version)
+  CorsikaSimtelPack = 'corsika_simhessarray/' + version + '/corsika_simhessarray' 
+  cs = CorsikaApp()
+  cs.setSoftwarePackage(CorsikaSimtelPack)
+  cs.csExe = executable
+  cs.csArguments = ['--run-number',run_number,'--run','corsika',corsikaTemplate]
+  corsikaReturnCode = cs.execute()
+  
+  if corsikaReturnCode != 0:
+    DIRAC.gLogger.error( 'Corsika Application: Failed')
+    jobReport.setApplicationStatus('Corsika Application: Failed')
+    DIRAC.exit( -1 )
+
+###################### rename of corsika output file #######################
+  rundir = 'run' + run_number
+  filein = rundir + '/' + corsikaOutputFileName
+  corsikaFileName = particle + '_' + thetaP + '_' + phiP + '_alt' + obslev + '_' + 'run' + run_number +  '.corsika.gz'
+  mv_cmd = 'mv ' + filein + ' ' + corsikaFileName
+  if(os.system(mv_cmd)):
+    DIRAC.exit( -1 )
+
+  ### create corsika tar luisa ####################
+  corsikaTarName = particle + '_' + thetaP + '_' + phiP + '_alt' + obslev + '_' + 'run' + run_number +  '.corsika.tar.gz'
+  filetar1 = rundir + '/'+'input'
+  filetar2 = rundir + '/'+ 'DAT' + run_number + '.dbase'
+  filetar3 = rundir + '/run' + str(int(run_number)) + '.log'
+  cmdTuple = ['/bin/tar','zcf',corsikaTarName, filetar1,filetar2,filetar3]
+  DIRAC.gLogger.notice( 'Executing command tuple:', cmdTuple )
+  ret = systemCall( 0, cmdTuple, sendOutput)
+  if not ret['OK']:
+    DIRAC.gLogger.error( 'Failed to execute tar')
+    DIRAC.exit( -1 )
+
+  if (mode == 'corsika_standalone'):
+    DIRAC.exit()
+
+############ Producing SimTel File
+ ######################Building simtel Directory Metadata #######################
+
+  cfg_dict = {"4MSST":'cta-prod2-4m-dc',"SCSST":'cta-prod2-sc-sst',"STD":'cta-prod2',"NSBX3":'cta-prod2',"ASTRI":'cta-prod2-astri',"NORTH":'cta-prod2n'}
+
+  #if simtelConfig=="6INROW":
+  #  all_configs=["4MSST","SCSST","ASTRI","NSBX3","STD","SCMST"]
+  #elif simtelConfig=="5INROW":
+  #  all_configs=["4MSST","SCSST","ASTRI","NSBX3","STD"]
+  #else:
+  #  all_configs=[simtelConfig]
+
+  all_configs=[simtelConfig]
+
+  for current_conf in all_configs:
+
+    DIRAC.gLogger.notice('current conf is',current_conf)
+    current_version = version
+    DIRAC.gLogger.notice('current version is', current_version)
+
+    #if current_conf == "SCMST":
+    #  current_version = version + '_sc3'
+    #  DIRAC.gLogger.notice('current version is', current_version)
+    #  if os.path.isdir('sim_telarray'):
+    #    DIRAC.gLogger.notice('Package found in the local area. Removing package...')
+    #    cmd = 'rm -R sim_telarray corsika-6990 hessioxxx corsika-run'
+    #    if(os.system(cmd)):
+    #      DIRAC.exit( -1 )
+    #    install_CorsikaSimtelPack(current_version)
+    #else:
+     # current_version = version
+     # DIRAC.gLogger.notice('current version is', current_version)
+
+#### execute simtelarray ################
+    fd = open('run_sim.sh', 'w' )
+    fd.write( """#! /bin/sh  
+    export SVNPROD2=$PWD
+    export SVNTAG=SVN-PROD2
+    export CORSIKA_IO_BUFFER=800MB
+    ./grid_prod2-repro.sh %s %s""" % (corsikaFileName,current_conf))
+    fd.close()
+####################################
+
+    os.system('chmod u+x run_sim.sh')
+    cmdTuple = ['./run_sim.sh']
+    ret = systemCall( 0, cmdTuple, sendSimtelOutput)
+    simtelReturnCode, stdout, stderr = ret['Value']
+
+    if(os.system('grep Broken simtel.log')==0):
+      DIRAC.gLogger.error('Broken string found in simtel.log')
+      jobReport.setApplicationStatus('Broken pipe')
+      DIRAC.exit( -1 )
+
+    if not ret['OK']:
+      DIRAC.gLogger.error( 'Failed to execute run_sim.sh')
+      DIRAC.gLogger.error( 'run_sim.sh status is:', simtelReturnCode)
+      DIRAC.exit( -1 )
+
+##   check simtel data/log/histo Output File exist
+    cfg = cfg_dict[current_conf]
+    simtelFileName = particle + '_' + str(thetaP) + '_' + str(phiP) + '_alt' + str(obslev) + '_' + 'run' + run_number + '.simtel.gz'
+    cmd = 'mv Data/sim_telarray/' + cfg + '/0.0deg/Data/*.simtel.gz ' + simtelFileName
+    if(os.system(cmd)):
+      DIRAC.exit( -1 )
+
+    simtelLogFileName = particle + '_' + str(thetaP) + '_' + str(phiP) + '_alt' + str(obslev) + '_' + 'run' + run_number + '.log.gz'
+    cmd = 'mv Data/sim_telarray/' + cfg + '/0.0deg/Log/*.log.gz ' + simtelLogFileName
+    if(os.system(cmd)):
+      DIRAC.exit( -1 )
+
+    simtelHistFileName = particle + '_' + str(thetaP) + '_' + str(phiP) + '_alt' + str(obslev) + '_' + 'run' + run_number + '.hdata.gz'
+    cmd = 'mv Data/sim_telarray/' + cfg + '/0.0deg/Histograms/*.hdata.gz ' + simtelHistFileName
+    if(os.system(cmd)):
+      DIRAC.exit( -1 )
+
+################################################################
+
+  DIRAC.exit()
+
+def install_CorsikaSimtelPack(version):
+
+  from CTADIRAC.Core.Utilities.SoftwareInstallation import checkSoftwarePackage
+  from CTADIRAC.Core.Utilities.SoftwareInstallation import installSoftwarePackage
+  from CTADIRAC.Core.Utilities.SoftwareInstallation import installSoftwareEnviron
+  from CTADIRAC.Core.Utilities.SoftwareInstallation import sharedArea
+  from CTADIRAC.Core.Utilities.SoftwareInstallation import workingArea
+  from DIRAC.Core.Utilities.Subprocess import systemCall
+
+
   CorsikaSimtelPack = 'corsika_simhessarray/' + version + '/corsika_simhessarray'
-
   packs = [CorsikaSimtelPack]
-
   for package in packs:
     DIRAC.gLogger.notice( 'Checking:', package )
     if sharedArea:
@@ -82,8 +207,8 @@ def main():
         DIRAC.gLogger.notice( 'Package found in Shared Area:', package )
         installSoftwareEnviron( package, workingArea() )
         packageTuple =  package.split('/')
-        corsika_subdir = sharedArea() + '/' + packageTuple[0] + '/' + version 
-        cmd = 'cp -u -r ' + corsika_subdir + '/* .'        
+        corsika_subdir = sharedArea() + '/' + packageTuple[0] + '/' + version  
+        cmd = 'cp -u -r ' + corsika_subdir + '/* .'       
         os.system(cmd)
         continue
     if workingArea:
@@ -92,94 +217,107 @@ def main():
         continue
       if installSoftwarePackage( package, workingArea() )['OK']:
       ############## compile #############################
-        if version == 'clean_23012012':
-          cmdTuple = ['./build_all','ultra','qgs2']
-        elif version in ['prod-2_21122012','prod-2_08032013']:
-          cmdTuple = ['./build_all','prod2','qgs2']
+        cmdTuple = ['./build_all','prod2','qgs2']
         ret = systemCall( 0, cmdTuple, sendOutput)
         if not ret['OK']:
           DIRAC.gLogger.error( 'Failed to execute build')
           DIRAC.exit( -1 )
         continue
-
     DIRAC.gLogger.error( 'Check Failed for software package:', package )
     DIRAC.gLogger.error( 'Software package not available')
     DIRAC.exit( -1 )  
 
-  cs = CorsikaApp()
-  cs.setSoftwarePackage(CorsikaSimtelPack)
+def createGlobalsFromConfigFiles(corsikaConfigFileName,version):
 
-###### execute corsika ###############
-  cs.csExe = executable
-  cs.csArguments = ['--run-number',run_number,'--run','corsika',template] 
-  corsikaReturnCode = cs.execute()
+  global prodName
+  global thetaP
+  global phiP
+  global particle
+  global energyInfo
+  global viewCone
+  global pathroot
+  global nbShowers
+  global cscat
+  global simtelOffset
+  global prodDirPath
+  global corsikaDirPath
+  global corsikaParticleDirPath
+  #global simtelDirPath
+  global corsikaOutputFileName
+  global corsikaProdVersion
+  #global simtelProdVersion
+  global obslev
+
+  # Getting MD Values from Config Files:
+  #prodKEYWORDS =  ['prodName','pathroot']
+  #dictProdKW = fileToKWDict(prodConfigFileName,prodKEYWORDS)
+
+  corsikaKEYWORDS = ['THETAP', 'PHIP', 'PRMPAR', 'ESLOPE' , 'ERANGE', 'VIEWCONE','NSHOW','TELFIL','OBSLEV','CSCAT']
+  dictCorsikaKW = fileToKWDict(corsikaConfigFileName,corsikaKEYWORDS)
+
+  #simtelKEYWORDS = ['env offset']
+
+  # Formatting MD values retrieved in configFiles
+  #prodName = dictProdKW['prodName'][0]
+  corsikaProdVersion = version + '_corsika'
+  #simtelProdVersion = version + '_simtel'
+  thetaP = str(float(dictCorsikaKW['THETAP'][0]))
+  phiP = str(float(dictCorsikaKW['PHIP'][0]))
+  obslev = str(float(dictCorsikaKW['OBSLEV'][0])/100.)#why on earth is this in cm....
+  nbShowers = str(int(dictCorsikaKW['NSHOW'][0]))
+  cscat = str(int(dictCorsikaKW['CSCAT'][0]))
+  corsikaOutputFileName = dictCorsikaKW['TELFIL'][0]  
   
-  if corsikaReturnCode != 0:
-    DIRAC.gLogger.error( 'Failed to execute corsika Application')
-    jobReport.setApplicationStatus('Corsika Application: Failed')
-    DIRAC.exit( -1 )
-
-###### rename corsika file #################################
-  rundir = 'run' + run_number
-  corsikaKEYWORDS = ['TELFIL']
-  dictCorsikaKW = fileToKWDict(template,corsikaKEYWORDS)
-  corsikafilename = rundir + '/' + dictCorsikaKW['TELFIL'][0]
-  destcorsikafilename = 'corsika_run' + run_number + '.corsika.gz'
-  cmd = 'mv ' + corsikafilename + ' ' + destcorsikafilename
-  os.system(cmd)
-  DIRAC.gLogger.notice( 'Corsika File Size:' )
-  cmdTuple = ['/bin/ls','-l',destcorsikafilename]
-  ret = systemCall( 0, cmdTuple, sendOutput)
-  if not ret['OK']:
-    DIRAC.gLogger.error( 'Failed to execute ls for corsika file')
-    DIRAC.exit( -1 )
+  #building simtelArray Offset
+  dictSimtelKW={}
+  simtelOffset = '"0.0"'
   
-  ### create corsika tar ####################
-  corsika_tar = 'corsika_run' + run_number + '.tar.gz'
-  filetar1 = rundir + '/'+'input'
-  filetar2 = rundir + '/'+ 'DAT' + run_number + '.dbase'
-  filetar3 = rundir + '/run' + str(int(run_number)) + '.log'
-  cmdTuple = ['/bin/tar','zcf',corsika_tar, filetar1,filetar2,filetar3]
-  DIRAC.gLogger.notice( 'Executing command tuple:', cmdTuple )
-  ret = systemCall( 0, cmdTuple, sendOutput)
-  if not ret['OK']:
-    DIRAC.gLogger.error( 'Failed to execute tar')
-    DIRAC.exit( -1 )
+  #building viewCone
+  viewConeRange = dictCorsikaKW['VIEWCONE']
+  viewCone = str(float(viewConeRange[1]))
     
-  if (mode == 'corsika_standalone'):
-    DIRAC.exit()
+  #building ParticleName
+  dictParticleCode={}
+  dictParticleCode['1'] = 'gamma'
+  dictParticleCode['14'] = 'proton'
+  dictParticleCode['3'] = 'electron'
+  dictParticleCode['402'] = 'helium'
+  dictParticleCode['1407'] = 'nitrogen'
+  dictParticleCode['2814'] = 'silicon'
+  dictParticleCode['5626'] = 'iron'
+  particleCode = dictCorsikaKW['PRMPAR'][0]
+  particle = dictParticleCode[particleCode]
+  if viewCone=='0.0':
+    particle+="_ptsrc"
 
-###### execute sim_telarray ###############
-  fd = open('run_sim.sh', 'w' )
-  fd.write( """#! /bin/sh                                                                                                                         
-			echo "go for sim_telarray"
-			. ./examples_common.sh
-			zcat %s | $SIM_TELARRAY_PATH/%s""" % (destcorsikafilename,simexe))
-  fd.close()
+  #building energy info:
+  eslope = dictCorsikaKW['ESLOPE'][0]
+  eRange = dictCorsikaKW['ERANGE']
+  emin = eRange[0]
+  emax = eRange[1]
+  energyInfo = eslope + '_' + emin + '-' + emax
+  
+  #pathroot = dictProdKW['pathroot'][0]
+  #building full prod, corsika and simtel Directories path
+  #prodDirPath = os.path.join(pathroot,prodName)
+  #corsikaDirPath = os.path.join(prodDirPath,corsikaProdVersion)
+  #corsikaParticleDirPath = os.path.join(corsikaDirPath,particle)
+  #simtelDirPath = os.path.join(corsikaParticleDirPath,simtelProdVersion)
 
-  os.system('chmod u+x run_sim.sh')
-  cmdTuple = ['./run_sim.sh']
-  ret = systemCall( 0, cmdTuple, sendSimtelOutput)
-
-  if not ret['OK']:
-    DIRAC.gLogger.error( 'Failed to execute run_sim.sh')
-    DIRAC.exit( -1 )
-    
-  DIRAC.exit()
-
-#### parse corsika template ##############
 def fileToKWDict (fileName, keywordsList):    
-  DIRAC.gLogger.notice('parsing: ', fileName)
+  #print 'parsing %s...' % fileName
   dict={}
   configFile = open(fileName, "r").readlines()
   for line in configFile:
-    for word in line.split():
-      if word in keywordsList:
-        lineSplit = line.split()
-        lenLineSplit = len(lineSplit)
-        value = lineSplit[1:lenLineSplit]
-        dict[word] = value
+    if (len(line.split())>0):
+      for word in line.split():
+        if line.split()[0] is not '*' and word in keywordsList:
+          lineSplit = line.split()
+          lenLineSplit = len(lineSplit)
+          value = lineSplit[1:lenLineSplit]
+          dict[word] = value
   return dict
+
 
 if __name__ == '__main__':
 
@@ -188,5 +326,6 @@ if __name__ == '__main__':
   except Exception:
     DIRAC.gLogger.exception()
     DIRAC.exit( -1 )
+
 
 
