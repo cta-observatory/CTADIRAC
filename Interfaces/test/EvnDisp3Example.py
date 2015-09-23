e88b203d803f4b1961fb1a376914053676197e3c
Luisa Arrabito <arrabito@in2p3.fr>
2015-09-23 20:45:25 +0000

    minor change

--- Interfaces/test/EvnDisp3Example.py
+++ Interfaces/test/EvnDisp3Example.py
@@ -7,7 +7,7 @@ Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                      '  %s mode infile' % Script.scriptName,
                                      'Arguments:',
                                      '  infile: ascii file with input files LFNs',
-                                     '  mode: WMS, TS',
+                                     '  mode: WMS, TS (default)',
                                      '\ne.g: %s Paranal_gamma_North.list TS' % Script.scriptName,
                                      ] ) )
 
@@ -21,13 +21,12 @@ from DIRAC.Interfaces.API.Dirac import Dirac
 
 def submitTS( job, infileList ):
   """ Create a transformation executing the job workflow  """
-  # ## send jobs to Lyon
-  #job.setDestination( 'LCG.IN2P3-CC.fr' )
   t = Transformation()
   tc = TransformationClient()
   t.setType( "DataReprocessing" )
   t.setDescription( "EvnDisp3 example" )
   t.setLongDescription( "EvnDisplay analysis" )  # mandatory
+  t.setGroupSize(1)
   t.setBody ( job.workflow.toXML() )
 
   res = t.addTransformation()  # Transformation is created here
@@ -41,18 +40,25 @@ def submitTS( job, infileList ):
   transID = t.getTransformationID()
   tc.addFilesToTransformation( transID['Value'], infileList )  # Files added here
 
-  
   return res
 
-def submitWMS( job, infileList ):
+def submitWMS( job, infileList, nbFileperJob ):
   """ Submit the job locally or to the WMS  """
-# job.setDestination( 'LCG.IN2P3-CC.fr' )
-  dirac = Dirac()
-  # job.setInputData( infileList )
-  res = dirac.submit( job, "local" )
-# res = dirac.submit( job )
 
-  Script.gLogger.notice( 'Submission Result: ', res )
+  dirac = Dirac()
+  res = Dirac().splitInputData( infileList, nbFileperJob )
+  if not res['OK']:
+    Script.gLogger.error( 'Failed to splitInputData' )
+    DIRAC.exit( -1 )
+
+  job.setGenericParametricInput( res['Value'] )
+  job.setInputData( '%s' )
+
+  job.setJobGroup( 'EvnDisp-proton-partx09' )
+
+  res = dirac.submit( job )
+
+  Script.gLogger.info( 'Submission Result: ', res['Value'] )
   return res
 
 #########################################################
@@ -74,27 +80,34 @@ def runEvnDisp3( args = None ):
     if line != "\n":
       infileList.append( infile )
 
-  mode = args[1]
-  ### Main Script ###
-  job = EvnDisp3Job()
+  if len( args ) == 1:
+    mode = 'TS'
+  else:
+    mode = args[1]
 
+##################################
+  job = EvnDisp3Job(cpuTime = 432000)  # to be adjusted!!
+
+  ### Main Script ###
   # override for testing
   job.setName( 'EvnDisp3Test' )
   
   # package and version
   job.setPackage( 'evndisplay' )
-  job.setVersion( 'prod3_d20150831b' )
+  #job.setVersion( 'prod3_d20150903b' )
+  job.setVersion( 'prod3_d20150918' ) ### version with less verbose output
 
   # set EvnDisp Meta data
   job.setEvnDispMD( infileList[0] )
 
   # # set layout and telescope combination
-  job.setLayoutList( "3HB1" )
+  job.setLayoutList( "3HB1 3HB2 3HB3 3HD1 3HD2 3HI1" )
   job.setTelescopetypeCombinationList( "FA NA FG NG FD ND" )
   #  set calibration file and parameters file
   job.setCalibrationFile( 'prod3.peds.20150820.dst.root' )
   job.setReconstructionParameter( 'EVNDISP.prod3.reconstruction.runparameter.NN' )
   job.setNNcleaninginputcard( 'EVNDISP.NNcleaning.dat' )
+  
 
   job.setOutputSandbox( ['*Log.txt'] )
 
@@ -104,12 +117,12 @@ def runEvnDisp3( args = None ):
   if mode == 'TS':
     res = submitTS( job, infileList )
   elif mode == 'WMS':
-    res = submitWMS( job, infileList )
+    res = submitWMS( job, infileList, 10 )
   else:
     Script.showHelp()
     
   # debug
-  Script.gLogger.info( job.workflow )
+    #Script.gLogger.info( job.workflow )
 
   return res
 
@@ -117,7 +130,7 @@ def runEvnDisp3( args = None ):
 if __name__ == '__main__':
 
   args = Script.getPositionalArgs()
-  if ( len( args ) != 2 ):
+  if ( len( args ) < 1):
     Script.showHelp()
   try:
     res = runEvnDisp3( args )
