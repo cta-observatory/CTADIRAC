0f20d9ce2359505dda73ee50e1da6ee2f7d64705
Luisa Arrabito <arrabito@in2p3.fr>
2015-09-23 08:58:59 +0000

    add new test script to verify prod3 steps

---
+++ Core/scripts/cta-prod3-verifysteps2.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+""" Collection of simple functions to verify each of
+    the main Prod3MCJob steps
+    
+         verifySteps.py JB, LA 2015
+"""
+
+# generic imports
+import os, glob
+
+# DIRAC imports
+import DIRAC
+from DIRAC.Core.Base import Script
+ 
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s stepName' % Script.scriptName,
+                                     'Arguments:',
+                                     '  stepName: corsika, simtel, merging',
+                                     '\ne.g: %s corsika' % Script.scriptName
+                                     ] ) )
+
+Script.parseCommandLine()
+
+def cleanFiles( outputFiles ):
+    """ Delete Local Files
+    """
+    DIRAC.gLogger.info( 'Deleting Local Files' )
+
+    for afile in outputFiles:
+      DIRAC.gLogger.warn( 'Remove local File %s' % ( afile ) )
+      os.remove( afile )
+
+    return DIRAC.S_OK()
+
+def verifyCorsika( nbFiles = 6, minSize = 50. ):
+    """ Verify a PROD3 corsika step
+    
+    Keyword arguments:
+    nbFiles -- number of output files expected
+    minSize -- minimum file size
+    """
+    DIRAC.gLogger.info('Verifying Corsika step')
+
+    # get list of output files
+    outputFiles=glob.glob('Data/corsika/run*/*corsika.gz')
+    
+    # check the number of output files
+    N=len(outputFiles)
+    if N != nbFiles :
+        res = DIRAC.S_ERROR()
+        res['Message'] = 'Wrong number of Corsika files : %s instead of %s' % (N, nbFiles)
+        cleanFiles( outputFiles )
+        return res
+        
+    # check the file size
+    for afile in outputFiles:
+        sizekb=os.path.getsize(afile)/1024.
+        if sizekb < minSize:
+            res = DIRAC.S_ERROR()
+            res['Message'] = '%s\n File size too small : %s < %s kb'% (afile, sizekb, minSize)
+            cleanFiles( outputFiles )
+            return res
+
+    return DIRAC.S_OK()
+
+
+def verifySimtel(nbFiles=31, minSize=50.):
+    """ Verify a PROD3 simtel step
+    
+    Keyword arguments:
+    nbFiles -- number of output files expected
+    minSize -- minimum file size
+    """
+    DIRAC.gLogger.info('Verifying Simtel step')
+    # get list of output files
+    outputFiles=glob.glob('Data/simtel_tmp/Data/*simtel.gz')
+    
+    # check the number of output files --- could be done by telescope type
+    N=len(outputFiles)
+    if N != nbFiles :
+        res = DIRAC.S_ERROR()
+        res['Message'] = 'Wrong number of Simtel files : %s instead of %s' % (N, nbFiles)
+        cleanFiles( outputFiles )
+        return res
+        
+    # check the file size --- could be done by telescope type
+    for afile in outputFiles:
+        sizekb=os.path.getsize(afile)/1024.
+        if sizekb < minSize:
+            res = DIRAC.S_ERROR()
+            res['Message'] = '%s\n File size too small : %s < %s kb'% (afile, sizekb, minSize)
+            cleanFiles( outputFiles )
+            return res
+
+    return DIRAC.S_OK()
+
+
+def verifyMerging(nbFiles=10, minSize=5000.):
+    """ Verify a PROD3 simtel merging step
+    
+    Keyword arguments:
+    nbFiles -- number of output files expected
+    minSize -- minimum file size
+    """
+    DIRAC.gLogger.info('Verifying Merging step')
+    
+    # get list of output files
+    outputFiles=glob.glob('Data/sim_telarray/cta-prod3/0.0deg/Data/*simtel.gz')
+    
+    # check the number of output files --- could be done by telescope type
+    N=len(outputFiles)
+    if N != nbFiles :
+        res = DIRAC.S_ERROR()
+        res['Message'] = 'Wrong number of Simtel Merged files : %s instead of %s' % (N, nbFiles)
+        cleanFiles( outputFiles )
+        return res
+        
+    # check the file size --- could be done by telescope type
+    for afile in outputFiles:
+        sizekb=os.path.getsize(afile)/1024.
+        if sizekb < minSize:
+            res = DIRAC.S_ERROR()
+            res['Message'] = '%s\n File size too small : %s < %s kb'% (afile, sizekb, minSize)
+            cleanFiles( outputFiles )
+            return res
+            
+    return DIRAC.S_OK()
+
+def verifyEvnDispInputs( minSize = 50. ):
+    """ Verify a EvnDisp input files
+
+    Keyword arguments:
+    minSize -- minimum file size
+    """
+    DIRAC.gLogger.info( 'Verifying EvnDispInputs step' )
+
+    # get list of output files
+    outputFiles = glob.glob( './*simtel.gz' )
+
+    # check the file size --- could be done by telescope type
+    for afile in outputFiles:
+        sizekb = os.path.getsize( afile ) / 1024.
+        if sizekb < minSize:
+            DIRAC.gLogger.warn( '%s\n File size too small : %s < %s kb' % ( afile, sizekb, minSize ) )
+            DIRAC.gLogger.warn( 'Remove local File %s' % ( afile ) )
+            os.remove( afile )
+
+    return DIRAC.S_OK()
+
+# Main
+def verify(args):
+    """ simple wrapper to put and register all PROD3 files
+    
+    Keyword arguments:
+    args -- a list of arguments in order []
+    """
+    stepType = args[0] 
+    res=None
+    # What shall we verify ?
+    if stepType == "corsika":
+        res=verifyCorsika()
+    elif stepType == "simtel":
+        res=verifySimtel()
+    elif stepType == "merging":
+        res=verifyMerging()
+    else:
+        res=DIRAC.S_ERROR()
+        res['Message'] = 'Do not know how to verify "%s"'% stepType
+           
+    return res
+
+####################################################
+if __name__ == '__main__':
+  
+  DIRAC.gLogger.setLevel('VERBOSE')
+  args = Script.getPositionalArgs()
+  try:    
+    res = verify( args )
+    if not res['OK']:
+      DIRAC.gLogger.error ( res['Message'] )
+      DIRAC.exit( -1 )
+    else:
+      DIRAC.gLogger.notice( 'Done' )
+  except Exception:
+    DIRAC.gLogger.exception()
+    DIRAC.exit( -1 )
