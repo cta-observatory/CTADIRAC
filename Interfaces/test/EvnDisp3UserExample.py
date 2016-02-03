43a81b0370cb2c855f8b4af59424a72667aedfbe
Luisa Arrabito <arrabito@in2p3.fr>
2016-02-03 08:19:59 +0000

    minor change

---
+++ Interfaces/test/EvnDisp3UserExample.py
@@ -0,0 +1,105 @@
+""" Prod3 EvnDisp Script for users
+"""
+
+from DIRAC.Core.Base import Script
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s infile' % Script.scriptName,
+                                     'Arguments:',
+                                     '  infile: ascii file with input files LFNs',
+                                     '\ne.g: %s Paranal_gamma_North.list' % Script.scriptName,
+                                     ] ) )
+
+Script.parseCommandLine()
+
+import DIRAC
+from CTADIRAC.Interfaces.API.EvnDisp3UserJob import EvnDisp3UserJob
+from DIRAC.Interfaces.API.Dirac import Dirac
+
+
+def submitWMS( job, infileList, nbFileperJob ):
+  """ Submit the job to the WMS  """
+
+  dirac = Dirac()
+  res = Dirac().splitInputData( infileList, nbFileperJob )
+  if not res['OK']:
+    Script.gLogger.error( 'Failed to splitInputData' )
+    DIRAC.exit( -1 )
+
+  job.setGenericParametricInput( res['Value'] )
+  job.setInputData( '%s' )
+  job.setOutputData( ['./*evndisp.tar.gz'] ) # to be used if DataManagement step in EvnDisp3UserJob is commented
+
+  #job.setJobGroup( 'EvnDisp-proton' )
+
+  res = dirac.submit( job )
+
+  Script.gLogger.info( 'Submission Result: ', res['Value'] )
+  return res
+
+#########################################################
+
+def runEvnDisp3( args = None ):
+  """ Simple wrapper to create a EnDisp3Job and setup parameters
+      from positional arguments given on the command line.
+      
+      Parameters:
+      args -- infile mode
+  """
+  # get arguments
+  infile = args[0]
+  f = open( infile, 'r' )
+
+  infileList = []
+  for line in f:
+    infile = line.strip()
+    if line != "\n":
+      infileList.append( infile )
+
+##################################
+  job = EvnDisp3UserJob(cpuTime = 432000)  # to be adjusted!!
+
+  ### Main Script ###
+  job.setName( 'EvnDisp3Test' )
+  
+  # package and version
+  job.setPackage( 'evndisplay' )
+  job.setVersion( 'prod3_d20160111' )
+
+  # # set layout list
+  job.setLayoutList( "CTA.prod3S.3HB1-NA.lis CTA.prod3S.3HB2-NA.lis" )
+  #  set calibration file and parameters file
+  job.setCalibrationFile( 'prod3.peds.20150820.dst.root' )
+  job.setReconstructionParameter( 'EVNDISP.prod3.reconstruction.runparameter.NN' )
+  job.setNNcleaninginputcard( 'EVNDISP.NNcleaning.dat' )
+  
+
+  job.setOutputSandbox( ['*Log.txt'] )
+  job.setInputSandbox( ['myconf'] )
+
+  # add the sequence of executables
+  job.setupWorkflow()
+
+  res = submitWMS( job, infileList, 2 )
+
+  # debug
+    #Script.gLogger.info( job.workflow )
+
+  return res
+
+#########################################################
+if __name__ == '__main__':
+
+  args = Script.getPositionalArgs()
+  if ( len( args ) < 1):
+    Script.showHelp()
+  try:
+    res = runEvnDisp3( args )
+    if not res['OK']:
+      DIRAC.gLogger.error ( res['Message'] )
+      DIRAC.exit( -1 )
+    else:
+      DIRAC.gLogger.notice( res['Value'] )
+  except Exception:
+    DIRAC.gLogger.exception()
+    DIRAC.exit( -1 )
