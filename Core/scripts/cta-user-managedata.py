ac2931ac7b873927c12e387e14374d786f77dea0
Luisa Arrabito <arrabito@in2p3.fr>
2016-02-04 13:46:17 +0000

    add cta-user-managedata.py

---
+++ Core/scripts/cta-user-managedata.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+""" Simple data management script for users
+    put and register files in DFC
+"""
+
+__RCSID__ = "$Id$"
+
+# generic imports
+import os, glob
+
+# DIRAC imports
+import DIRAC
+from DIRAC.Core.Base import Script
+Script.parseCommandLine()
+
+# Specific DIRAC imports
+from CTADIRAC.Core.Workflow.Modules.Prod3DataManager import Prod3DataManager
+
+####################################################
+def putAndRegisterPROD3( args ):
+    """ simple wrapper to put and register all analysis files
+
+    Keyword arguments:
+    args -- a list of arguments in order []
+    """
+    outputpattern = args[0]
+    outputpath = args[1]
+    outputSE = args[2]
+
+    catalogs = ['DIRACFileCatalog']
+
+    # # Init DataManager
+    prod3dm = Prod3DataManager( catalogs )
+
+    # # Upload data files
+    res = prod3dm._checkemptydir( outputpattern )
+    if not res['OK']:
+      return res
+
+    for localfile in glob.glob( outputpattern ):
+      filename = os.path.basename( localfile )
+      lfn = os.path.join( outputpath, filename )
+      res = prod3dm._putAndRegisterToSEList( lfn, localfile, outputSE )
+      if not res['OK']:
+        return res
+
+    return DIRAC.S_OK()
+
+####################################################
+if __name__ == '__main__':
+  args = Script.getPositionalArgs()
+  try:
+    res = putAndRegisterPROD3( args )
+    if not res['OK']:
+      DIRAC.gLogger.error ( res['Message'] )
+      DIRAC.exit( -1 )
+    else:
+      DIRAC.gLogger.notice( 'Done' )
+  except Exception:
+    DIRAC.gLogger.exception()
+    DIRAC.exit( -1 )
