65fc89f3e718ba09e98670ce265d603236d3b64b
Luisa Arrabito <arrabito@in2p3.fr>
2016-02-10 09:50:56 +0000

    bug fix

--- Core/scripts/cta-user-managedata.py
+++ Core/scripts/cta-user-managedata.py
@@ -15,6 +15,7 @@ Script.parseCommandLine()
 
 # Specific DIRAC imports
 from CTADIRAC.Core.Workflow.Modules.Prod3DataManager import Prod3DataManager
+from DIRAC.DataManagementSystem.Client.DataManager import DataManager
 
 ####################################################
 def putAndRegisterPROD3( args ):
@@ -25,9 +26,10 @@ def putAndRegisterPROD3( args ):
     """
     outputpattern = args[0]
     outputpath = args[1]
-    outputSE = args[2]
+    SE = args[2]
 
     catalogs = ['DIRACFileCatalog']
+    dm = DataManager( catalogs )
 
     # # Init DataManager
     prod3dm = Prod3DataManager( catalogs )
@@ -40,8 +42,15 @@ def putAndRegisterPROD3( args ):
     for localfile in glob.glob( outputpattern ):
       filename = os.path.basename( localfile )
       lfn = os.path.join( outputpath, filename )
-      res = prod3dm._putAndRegisterToSEList( lfn, localfile, outputSE )
+      msg = 'Try to upload local file: %s \nwith LFN: %s \nto %s' % ( localfile, lfn, SE )
+      DIRAC.gLogger.notice( msg )
+      res = dm.putAndRegister( lfn, localfile, SE )
+      # ##  check if failed
       if not res['OK']:
+        DIRAC.gLogger.error( 'Failed to putAndRegister %s \nto %s \nwith message: %s' % ( lfn, SE, res['Message'] ) )
+        return res
+      elif res['Value']['Failed'].has_key( lfn ):
+        DIRAC.gLogger.error( 'Failed to putAndRegister %s to %s' % ( lfn, SE ) )
         return res
 
     return DIRAC.S_OK()
