763a357558d1672e477df8372109063f4a89f193
Johan Bregeon <johan.bregeon@lupm.in2p3.fr>
2016-05-03 18:12:23 +0000

    testing

--- Interfaces/API/Prod3Merge3HB89Job.py
+++ Interfaces/API/Prod3Merge3HB89Job.py
@@ -104,7 +104,7 @@ class Prod3Merge3HB89Job( Job ) :
 
     # step 4 - download subarray-2 files corresponding to sub-1 input
 #    rctaStep = self.setExecutable( '$DIRACROOT/scripts/cta-prod3-get-matching-data sub2sub5',\
-    rctaStep = self.setExecutable( './cta-prod3-get-matching-data sub2sub5',\
+    rctaStep = self.setExecutable( 'python ./cta-prod3-get-matching-data.py sub2sub5',\
                                 logFile = 'Download_Files_Log.txt' )
     rctaStep['Value']['name'] = 'Step%i_Download_Files' % iStep
     rctaStep['Value']['descr_short'] = 'Download subarray-2 and 5 Files'
