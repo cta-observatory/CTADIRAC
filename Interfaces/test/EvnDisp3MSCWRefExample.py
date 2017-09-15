db8bf0801e71c5dfddb44d6077d2573d7528c485
BREGEON Johan <johan.bregeon@lupm.in2p3.fr>
2017-09-15 11:37:26 +0200

    fix defaults, table 180deg for South pointing, groupSize=100

--- Interfaces/test/EvnDisp3MSCWRefExample.py
+++ Interfaces/test/EvnDisp3MSCWRefExample.py
@@ -26,7 +26,7 @@ def submitTSold( job, transName, mqJson ):
 
   # Initialize JOB_ID
   job.workflow.addParameter( Parameter( "JOB_ID", "000000", "string", "", "", True, False, "Temporary fix" ) )
-  
+
   tc = TransformationClient()
 
   res = tc.addTransformation( transName, 'EvnDisp3MSCW example',
@@ -46,21 +46,21 @@ def submitTSold( job, transName, mqJson ):
 
 def submitTS( job, transName, mqJson ):
     """ Create a transformation executing the job workflow
-    This is using a file mask so that files be added on the fly.    
+    This is using a file mask so that files be added on the fly.
     """
     DIRAC.gLogger.notice( 'submitTS' )
 
     # Initialize JOB_ID
     job.workflow.addParameter( Parameter( "JOB_ID", "000000", "string", "", "",
                                        True, False, "Temporary fix" ) )
-   
+
     t = Transformation( )
     t.setTransformationName(transName) # this must be unique
     t.setType("DataReprocessing")
     t.setDescription("EvnDisp3MSCW example")
-    t.setLongDescription( "EvnDisplay stereo reconstruction") # mandatory    
-    t.setBody(job.workflow.toXML())    
-    t.setGroupSize(10)
+    t.setLongDescription( "EvnDisplay stereo reconstruction") # mandatory
+    t.setBody(job.workflow.toXML())
+    t.setGroupSize(100)
     t.setFileMask(mqJson) # catalog query is defined here
     t.addTransformation() # transformation is created here
     t.setStatus("Active")
@@ -97,11 +97,11 @@ def runEvnDisp3MSCW( args = None ):
 
   # change here for Paranal or La Palma
   job.setPrefix( "CTA.prod3Nb" )
-  job.setPointing(180)
+  job.setPointing(180) # must match table
   job.setDispSubDir('BDTdisp.Nb.3AL4-BN15.T1')
   job.setRecId('0,1,2') # 0 = all teltescopes, 1 = LST only, 2 = MST only
-  #  set calibration file and parameters file
-  job.setTableFile( 'tables_CTA-prod3b-LaPalma-NNq05-NN-ID0_0deg-d20160925m4-Nb.3AL4-BN15.root' ) # for La Palma
+  #  set calibration file and parameters file, must match pointing
+  job.setTableFile( 'tables_CTA-prod3b-LaPalma-NNq05-NN-ID0_180deg-d20160925m4-Nb.3AL4-BN15.root' )
 
 
   ### set meta-data to the product of the transformation
@@ -110,7 +110,7 @@ def runEvnDisp3MSCW( args = None ):
             'array_layout':'Baseline', 'site':'LaPalma',
             'outputType':'Data', 'data_level':{"=": 1},
             'configuration_id':{"=": 0},
-            'calibimgreco_prog':'evndisp', 
+            'calibimgreco_prog':'evndisp',
             'calibimgreco_prog_version':'prod3b_d20170602',
             'thetaP':{"=": 20}, 'phiP':{"=": 0.0}}
   job.setEvnDispMD( MDdict )
@@ -133,6 +133,5 @@ if __name__ == '__main__':
   args = Script.getPositionalArgs()
   if ( len( args ) != 1 ):
     Script.showHelp()
-  
+
   runEvnDisp3MSCW( args )
-    
