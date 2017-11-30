f2355d50272a7f949aee7705228004e77f4d37b2
BREGEON Johan <johan.bregeon@lupm.in2p3.fr>
2017-11-30 13:18:52 +0100

    pep8 and prep for LaPalma NSB

--- Interfaces/test/Prod3MCPipeBaselineNSBExample.py
+++ Interfaces/test/Prod3MCPipeBaselineNSBExample.py
@@ -3,7 +3,7 @@
 """
 
 from DIRAC.Core.Base import Script
-Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+Script.setUsageMessage('\n'.join([__doc__.split('\n')[1],
                                      'Usage:',
                                      '  %s array_layout site particle pointing_dir zenith_angle nShower' % Script.scriptName,
                                      'Arguments:',
@@ -14,7 +14,7 @@ Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                      '  zenith_agle: 20',
                                      '  nShower: from 5 to 25000',
                                      '\ne.g: %s Baseline Paranal gamma South 20 5'% Script.scriptName,
-                                     ] ) )
+                                 ]))
 
 Script.parseCommandLine()
 
@@ -22,110 +22,118 @@ import DIRAC
 from DIRAC.TransformationSystem.Client.Transformation import Transformation
 from DIRAC.Core.Workflow.Parameter import Parameter
 from CTADIRAC.Interfaces.API.Prod3MCPipeBaselineNSBJob import Prod3MCPipeBaselineNSBJob
-#from Prod3MCPipeBaselineJob import Prod3MCPipeBaselineJob
-#from DIRAC.Interfaces.API.Dirac import Dirac
+#from Prod3MCPipeBaselineNSBJob import Prod3MCPipeBaselineNSBJob
 
-def submitTS( job ):
-  """ Create a transformation executing the job workflow  """
 
-  ### Temporary fix to initialize JOB_ID #######
-  job.workflow.addParameter( Parameter( "JOB_ID", "000000", "string", "", "", True, False, "Temporary fix" ) )
-  job.workflow.addParameter( Parameter( "PRODUCTION_ID", "000000", "string", "", "", True, False, "Temporary fix" ) )
-  job.setType('MCSimulation') ## Used for the JobType plugin
+def submitTS(job):
+    """ Create a transformation executing the job workflow
+    """
+    # Temporary fix to initialize JOB_ID #######
+    job.workflow.addParameter(Parameter("JOB_ID", "000000", "string", "", "",
+                                        True, False, "Temporary fix"))
+    job.workflow.addParameter(Parameter("PRODUCTION_ID", "000000", "string",
+                                        "", "", True, False, "Temporary fix"))
+    job.setType('MCSimulation')  # Used for the JobType plugin
 
-  t = Transformation()
-  # t.setTransformationName( "Prod3Exemple" )  # This must be unique. If not set it's asked in the prompt
-  t.setType( "MCSimulation" )
-  t.setDescription( "MC Prod3 BaseLine NSB test" )
-  t.setLongDescription( "corsika-simtel production" )  # mandatory
-  t.setBody ( job.workflow.toXML() )
+    t = Transformation()
+    # This must be unique. If not set it's asked in the prompt
+    # t.setTransformationName("Prod3Exemple")
+    t.setType("MCSimulation")
+    t.setDescription("MC Prod3 BaseLine NSB test")
+    t.setLongDescription("corsika-simtel production")  # mandatory
+    t.setBody(job.workflow.toXML())
 
-  res = t.addTransformation()  # Transformation is created here
+    res = t.addTransformation()  # Transformation is created here
 
-  if not res['OK']:
-    print(res['Message'])
-    DIRAC.exit( -1 )
+    if not res['OK']:
+        print(res['Message'])
+        DIRAC.exit(-1)
 
-  t.setStatus( "Active" )
-  t.setAgentType( "Automatic" )
+    t.setStatus("Active")
+    t.setAgentType("Automatic")
 
-  return res
+    return res
 
 #########################################################
 
-def runProd3( args = None ):
-  """ Simple wrapper to create a Prod3MCPipeBaselineJob and setup parameters
-      from positional arguments given on the command line.
 
-      Parameters:
-      args -- a list of 6 strings corresponding to job arguments
-              array_layout site particle pointing_dir zenith_angle nShower
-              demo LaPalma  gamma South 20 1000
-  """
-  # get arguments
-  layout = args[0]
-  site = args[1]
-  particle = args[2]
-  pointing = args[3]
-  zenith = args[4]
-  nShower= args[5]
+def runProd3(args=None):
+    """ Simple wrapper to create a Prod3MCPipeBaselineJob and setup parameters
+    from positional arguments given on the command line.
 
-    ### Main Script ###
-  job = Prod3MCPipeBaselineNSBJob()
+    Parameters:
+    args -- a list of 6 strings corresponding to job arguments
+    array_layout site particle pointing_dir zenith_angle nShower
+    demo LaPalma  gamma South 20 1000
+    """
+    # get arguments
+    layout = args[0]
+    site = args[1]
+    particle = args[2]
+    pointing = args[3]
+    zenith = args[4]
+    nShower = args[5]
 
-  # override for testing
-  job.setName('BL_NSB_Test_Paranal_20deg_%s'%particle)
+    # Main Script ###
+    job = Prod3MCPipeBaselineNSBJob()
 
-  # package and version
-  job.setPackage('corsika_simhessarray')
-  job.setVersion( '2017-09-01' )  # final with fix for gamma-diffuse
-  job.configuration_id=1 # 0 exists already
+    # override for testing
+    job.setName('BL_NSB_LaPalma_20deg_%s' % particle)
 
-  # layout, site, particle, pointing direction, zenith angle
-  # demo, LaPalma,  gamma, South,  20
-  job.setArrayLayout(layout)
-  job.setSite(site)
-  job.setParticle(particle)
-  job.setPointingDir(pointing)
-  job.setZenithAngle( zenith )
+    # package and version
+    job.setPackage('corsika_simhessarray')
+    job.setVersion('2017-09-01-p1')  # final with fix for gamma-diffuse
+    job.configuration_id = 1  # 0 exists already
+    job.N_output_files = 2  # La Palma
 
-  # 5 is enough for testing
-  job.setNShower(nShower)
+    # layout, site, particle, pointing direction, zenith angle
+    # demo, LaPalma,  gamma, South,  20
+    job.setArrayLayout(layout)
+    job.setSite(site)
+    job.setParticle(particle)
+    job.setPointingDir(pointing)
+    job.setZenithAngle(zenith)
 
-  ### Set the startrunNb here (it will be added to the Task_ID)
-  startrunNb = '0'
-  job.setStartRunNumber( startrunNb )
+    # 5 is enough for testing
+    job.setNShower(nShower)
 
-  # set run number for TS submission: JOB_ID variable left for dynamic resolution during the Job. It corresponds to the Task_ID
-  job.setRunNumber( '@{JOB_ID}' )
+    # Set the startrunNb here (it will be added to the Task_ID)
+    startrunNb = '0'
+    job.setStartRunNumber(startrunNb)
 
-  # get dirac log files
-  job.setOutputSandbox( ['*Log.txt'] )
+    # set run number for TS submission: JOB_ID variable left for
+    # dynamic resolution during the Job. It corresponds to the Task_ID
+    job.setRunNumber('@{JOB_ID}')
 
-  # add the sequence of executables
-  job.setupWorkflow(debug=True)
+    # get dirac log files
+    job.setOutputSandbox(['*Log.txt'])
 
-  # submit to the Transformation System
-  res = submitTS( job )
+    # add the sequence of executables
+    job.setupWorkflow(debug=True)
 
-  # debug
-  Script.gLogger.info( job.workflow )
+    # submit to the Transformation System
+    res = submitTS(job)
 
-  return res
+    # debug
+    Script.gLogger.info(job.workflow)
+
+    return res
+
 
 #########################################################
 if __name__ == '__main__':
-
-  args = Script.getPositionalArgs()
-  if ( len( args ) != 6 ):
-    Script.showHelp()
-  try:
-    res = runProd3( args )
-    if not res['OK']:
-      DIRAC.gLogger.error ( res['Message'] )
-      DIRAC.exit( -1 )
-    else:
-      DIRAC.gLogger.notice( 'Done' )
-  except Exception:
-    DIRAC.gLogger.exception()
-    DIRAC.exit( -1 )
+    """ Do things
+    """
+    args = Script.getPositionalArgs()
+    if (len(args) != 6):
+        Script.showHelp()
+    try:
+        res = runProd3(args)
+        if not res['OK']:
+            DIRAC.gLogger.error(res['Message'])
+            DIRAC.exit(-1)
+        else:
+            DIRAC.gLogger.notice('Done')
+    except Exception:
+        DIRAC.gLogger.exception()
+        DIRAC.exit(-1)
