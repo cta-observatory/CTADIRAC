e49af3ba8b956f2ec9431ef925cbdb10c2d900d9
BREGEON Johan <johan.bregeon@lupm.in2p3.fr>
2018-02-15 14:20:01 +0100

    sync with Ref

--- Interfaces/API/EvnDisp3FinalJob.py
+++ Interfaces/API/EvnDisp3FinalJob.py
@@ -28,10 +28,12 @@ class EvnDisp3FinalJob(Job):
         Job.__init__(self)
         self.setCPUTime(cpuTime)
         # defaults
-        self.setName('Evndisplay_Analysis')
+        self.setName('Evndisplay_CalibReco')
         self.package='evndisplay'
-        self.program_category = 'analysis' #'calibimgreco'
+        self.program_category = 'calibimgreco'
         self.version = 'prod3b_d20180201'
+        self.configuration_id = -1
+        self.output_data_level = 1
         self.prefix = 'CTA.prod3S'
         self.layout_list = '3HB9-FD 3HB9-FG 3HB9-FA 3HB9-ND 3HB9-NG 3HB9-NA \
                             3HB9-TS-BB-FD 3HB9-TS-BB-FG 3HB9-TS-BB-FA \
@@ -41,36 +43,28 @@ class EvnDisp3FinalJob(Job):
         self.reconstructionparameter = 'EVNDISP.prod3.reconstruction.runparameter.NN.noLL'
         self.NNcleaninginputcard = 'EVNDISP.NNcleaning.dat'
         self.basepath = '/vo.cta.in2p3.fr/MC/PROD3/'
-        self.outputpattern = './*evndisp.tar.gz'
         self.fcc = FileCatalogClient()
         self.metadata = collections.OrderedDict()
         self.filemetadata = {}
         self.catalogs = json.dumps(['DIRACFileCatalog', 'TSCatalog'])
-        self.jobGroupID = 1
+        self.ts_task_id = 0
 
-    def setEvnDispMD(self, path):
-        """ Set evndisplay meta data starting from path metadata
+    def setEvnDispMD(self, simtelMD):
+        """ Set evndisplay meta data
 
         Parameters:
-        path -- path from which get meta data
+        simtelMD -- metadata dictionary as for simtel
         """
-        # # Get simtel meta data from path
-        res = self.fcc.getFileUserMetadata(path)
-        simtelMD = res['Value']
-
         # # Set evndisp directory meta data
         self.metadata['array_layout'] = simtelMD['array_layout']
         self.metadata['site'] = simtelMD['site']
         self.metadata['particle'] = simtelMD['particle']
-        self.metadata['phiP'] = simtelMD['phiP']
-        self.metadata['thetaP'] = simtelMD['thetaP']
-        # self.metadata['process_program'] = 'evndisp' + '_' + self.version
-        self.metadata['analysis_prog'] = 'evndisp'
-        self.metadata['analysis_prog_version'] = self.version
-        self.metadata['data_level'] = 1
-        self.metadata['configuration_id'] = -1
-        # here hardcode that we have no SCTs
-        self.metadata['sct'] = 'False'
+        self.metadata['phiP'] = simtelMD['phiP']['=']
+        self.metadata['thetaP'] = simtelMD['thetaP']['=']
+        self.metadata[self.program_category+'_prog'] = 'evndisp'
+        self.metadata[self.program_category+'_prog_version'] = self.version
+        self.metadata['data_level'] = self.output_data_level
+        self.metadata['configuration_id'] = self.configuration_id
 
     def setupWorkflow(self, debug=False):
         """ Setup job workflow by defining the sequence of all executables
@@ -106,10 +100,10 @@ class EvnDisp3FinalJob(Job):
         evStep = self.setExecutable('./dirac_prod3_evndisp_final',
                                     arguments = "--prefix %s --layout_list '%s' \
                                     --focal_file %s --calibration_file %s \
-                                    --reconstructionparameter %s" %
+                                    --reconstructionparameter %s  --taskid %s" %
                                     (self.prefix, self.layout_list,
                                      self.focal_file, self.calibration_file,
-                                     self.reconstructionparameter),
+                                     self.reconstructionparameter, self.ts_task_id),
                                     logFile='EvnDisp_Log.txt')
         evStep['Value']['name'] = 'Step%i_EvnDisplay' % iStep
         evStep['Value']['descr_short'] = 'Run EvnDisplay'
@@ -122,18 +116,44 @@ class EvnDisp3FinalJob(Job):
         metadatafield = {'array_layout':'VARCHAR(128)', 'site':'VARCHAR(128)',
                          'particle':'VARCHAR(128)',
                          'phiP':'float', 'thetaP': 'float',
-                         'analysis_prog':'VARCHAR(128)',
-                         'analysis_prog_version':'VARCHAR(128)'}
-
+                         self.program_category+'_prog':'VARCHAR(128)',
+		                 self.program_category+'_prog_version':'VARCHAR(128)',
+                         'data_level': 'int', 'configuration_id': 'int'}
         mdfieldjson = json.dumps(metadatafield)
+
+        # register Data
+        outputpattern = './*evndisp-DL%01d.tar.gz'%self.output_data_level
         file_md_json = json.dumps(self.filemetadata)
         scripts = '../CTADIRAC/Core/scripts'
         dmStep = self.setExecutable(scripts + '/cta-analysis-managedata.py',
                             arguments = "'%s' '%s' '%s' %s '%s' %s %s '%s'" %
                             (mdjson, mdfieldjson, file_md_json, self.basepath,
-                            self.outputpattern, self.package,
+                            outputpattern, self.package,
                             self.program_category, self.catalogs),
                             logFile='DataManagement_Log.txt')
         dmStep['Value']['name'] = 'Step%i_DataManagement' % iStep
         dmStep['Value']['descr_short'] = 'Save files to SE and register them in DFC'
         iStep += 1
+
+        # register Log
+        outputpattern = './*.logs.tgz'
+        filemetadata = {}
+        file_md_json = json.dumps(filemetadata)
+        dmStep = self.setExecutable('../CTADIRAC/Core/scripts/cta-analysis-managedata.py',
+                                  arguments = "'%s' '%s' '%s' %s '%s' %s %s '%s' Log" % \
+                                  (mdjson, mdfieldjson, file_md_json, self.basepath,
+                                   outputpattern, self.package, self.program_category, self.catalogs),
+                                  logFile = 'Log_DataManagement_Log.txt')
+        dmStep['Value']['name'] = 'Step%i_Log_DataManagement' % iStep
+        dmStep['Value']['descr_short'] = 'Save log files to SE and register them in DFC'
+        iStep += 1
+
+        # step 6 -- to be removed -- debug only
+        if debug:
+            lsStep = self.setExecutable('/bin/ls',
+                                        arguments = " -alhtr; /bin/ls -lahrt ./Data",
+                                        logFile = 'LS_End_Log.txt')
+            lsStep['Value']['name']='Step%i_LS_End'%iStep
+            lsStep['Value']['descr_short']='list files in working directory and in Data directory'
+            iStep+=1
+        
