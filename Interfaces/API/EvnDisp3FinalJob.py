cc7e9f9316f772355c07944a724dc08c913e44e4
BREGEON Johan <johan.bregeon@lupm.in2p3.fr>
2018-02-19 13:07:08 +0100

    fix setMD for MQ comes from dataset

--- Interfaces/API/EvnDisp3FinalJob.py
+++ Interfaces/API/EvnDisp3FinalJob.py
@@ -59,8 +59,8 @@ class EvnDisp3FinalJob(Job):
         self.metadata['array_layout'] = simtelMD['array_layout']
         self.metadata['site'] = simtelMD['site']
         self.metadata['particle'] = simtelMD['particle']
-        self.metadata['phiP'] = simtelMD['phiP']['=']
-        self.metadata['thetaP'] = simtelMD['thetaP']['=']
+        self.metadata['phiP'] = simtelMD['phiP']  # ['=']
+        self.metadata['thetaP'] = simtelMD['thetaP']  # ['=']
         self.metadata[self.program_category+'_prog'] = 'evndisp'
         self.metadata[self.program_category+'_prog_version'] = self.version
         self.metadata['data_level'] = self.output_data_level
@@ -156,4 +156,3 @@ class EvnDisp3FinalJob(Job):
             lsStep['Value']['name']='Step%i_LS_End'%iStep
             lsStep['Value']['descr_short']='list files in working directory and in Data directory'
             iStep+=1
-        
