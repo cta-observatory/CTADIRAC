0a83cdb83e504582120b99da6960913d10cee082
BREGEON Johan <johan.bregeon@lupm.in2p3.fr>
2018-10-17 14:40:25 +0200

    new Job wrapper and launcher to run simtel on PROD4 SST showers

---
+++ Interfaces/API/Prod4SimtelSSTJob.py
@@ -0,0 +1,155 @@
+"""
+  Wrapper on the Job class to handle Simtel Analysis of the PROD4 SST Corsika
+  showers
+
+  https://forge.in2p3.fr/issues/35179
+                        October 17th 2018 - J. Bregeon
+"""
+
+__RCSID__ = "$Id$"
+
+# generic imports
+import os, json, collections
+# DIRAC imports
+import DIRAC
+from DIRAC.Interfaces.API.Job import Job
+from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
+
+class Prod4SimtelSSTJob(Job):
+    """ Job extension class for Simtel Analysis
+
+    """
+    def __init__(self, cpuTime=432000):
+        """ Constructor
+
+        Keyword arguments:
+        cpuTime -- max cpu time allowed for the job
+        """
+        Job.__init__(self)
+        self.setCPUTime(cpuTime)
+        # defaults
+        self.setName('Simtel')
+        self.package='corsika_simhessarray'
+        self.program_category = 'tel_sim'
+        self.version = '2018-09-19'
+        self.configuration_id = 4
+        self.output_data_level = 0
+        self.N_output_files = 4
+        self.base_path = '/vo.cta.in2p3.fr/MC/PROD4/'
+        self.fcc = FileCatalogClient()
+        self.metadata = collections.OrderedDict()
+        self.catalogs = json.dumps(['DIRACFileCatalog', 'TSCatalog'])
+        self.ts_task_id = 0
+
+    def set_meta_data(self, corsika_md):
+        """ Set simtel meta data
+
+        Parameters:
+        corsika_md -- metadata dictionary of Corsika files
+        """
+        # # Set evndisp directory meta data
+        self.metadata['array_layout'] = corsika_md['array_layout']
+        self.metadata['site'] = corsika_md['site']
+        self.metadata['particle'] = corsika_md['particle']
+        try:
+            phiP = corsika_md['phiP']['=']
+        except:
+            phiP = corsika_md['phiP']
+        # invert phi from Corsika to Simtel as the reference systems is inverted
+        self.metadata['phiP'] = str((float(phiP)+180)%360)
+        try:
+            thetaP = corsika_md['thetaP']['=']
+        except:
+            thetaP = corsika_md['thetaP']
+        self.metadata['thetaP'] = thetaP
+        self.metadata[self.program_category+'_prog'] = 'simtel'
+        self.metadata[self.program_category+'_prog_version'] = self.version
+        self.metadata['data_level'] = self.output_data_level
+        self.metadata['configuration_id'] = self.configuration_id
+
+    def setupWorkflow(self, debug=False):
+        """ Setup job workflow by defining the sequence of all executables
+            All parameters shall have been defined before that method is called.
+        """
+        # step 1 -- debug
+        i_step = 1
+        if debug:
+            lsStep = self.setExecutable('/bin/ls -alhtr',
+                                        logFile='LS_Init_Log.txt')
+            lsStep['Value']['name'] = 'Step%i_LS_Init'%i_step
+            lsStep['Value']['descr_short'] = 'list files in working directory'
+            i_step+=1
+
+        # step 2
+        swStep = self.setExecutable('cta-prod3-setupsw',
+                                  arguments='%s %s'% (self.package,
+                                                      self.version),
+                                  logFile='SetupSoftware_Log.txt')
+        swStep['Value']['name'] = 'Step%i_SetupSoftware' % i_step
+        swStep['Value']['descr_short'] = 'Setup software'
+        i_step+=1
+
+        # step 3 - running
+        evStep = self.setExecutable('./dirac_prod4_sst_simtel',
+                                    arguments = "--taskid %s" % (self.ts_task_id),
+                                    logFile='Simtel_Log.txt')
+        evStep['Value']['name'] = 'Step%i_Simtel' % i_step
+        evStep['Value']['descr_short'] = 'Run Simtel'
+        i_step += 1
+
+        # step 4 verify merged data
+        mgvStep = self.setExecutable( '$DIRACROOT/scripts/cta-prod3-verifysteps', \
+                                  arguments = "generic %0d 1000 './*_data.tar'"%\
+                                              (self.N_output_files),\
+                                  logFile='Verify_Simtel_Log.txt')
+        mgvStep['Value']['name']='Step%i_VerifySimtel'%i_step
+        mgvStep['Value']['descr_short'] = 'Verify simtel files'
+        i_step += 1
+
+        # Step 5 - define meta data, upload file on SE and register in catalogs
+        md_json = json.dumps(self.metadata)
+        meta_data_field = {'array_layout': 'VARCHAR(128)', 'site': 'VARCHAR(128)',
+                           'particle': 'VARCHAR(128)',
+                           'phiP': 'float', 'thetaP': 'float',
+                           self.program_category + '_prog': 'VARCHAR(128)',
+                           self.program_category + '_prog_version': 'VARCHAR(128)',
+                           'data_level': 'int', 'configuration_id': 'int'}
+        md_field_json = json.dumps(meta_data_field)
+
+        # Upload and register data
+        tel_config_list = 'sst-1m sst-astri sst-astri+chec-s sst-gct'.split()
+        for config in tel_config_list:
+            tel_config = 'cta-prod4-%s' % config
+            file_meta_data = {'tel_config' : tel_config}
+            file_md_json = json.dumps(file_meta_data)
+            output_pattern = './*%s*_data.tar' % tel_config
+            scripts = '../CTADIRAC/Core/scripts/'
+            dm_step = self.setExecutable(scripts + 'cta-prod-managedata.py',
+                                         arguments="'%s' '%s' '%s' %s %s %s %s '%s' Data" %
+                                         (md_json, md_field_json, file_md_json,
+                                          self.base_path, output_pattern, self.package,
+                                          self.program_category, self.catalogs),
+                                         logFile='DataManagement_Log_%s.txt' % tel_config)
+            i_step += 1
+
+        # Upload and register log file
+        file_meta_data = {}
+        file_md_json = json.dumps(file_meta_data)
+        log_file_pattern = './*_log.tar'
+        scripts = '../CTADIRAC/Core/scripts/'
+        log_step = self.setExecutable(scripts + 'cta-prod-managedata.py',
+                                      arguments="'%s' '%s' '%s' %s %s %s %s '%s' Log" %
+                                      (md_json, md_field_json, file_md_json,
+                                       self.base_path, log_file_pattern, self.package,
+                                       self.program_category, self.catalogs),
+                                      logFile='LogManagement_Log.txt')
+        log_step['Value']['name'] = 'Step%s_LogManagement' % i_step
+        log_step['Value']['descr_short'] = 'Save log to SE and register them in DFC'
+        i_step += 1
+
+        # step 5 - debug only
+        if debug:
+            lsStep=self.setExecutable('/bin/ls -alhtr Data/sim_telarray/*/*/*',logFile='LS_End_Log.txt')
+            lsStep['Value']['name']='Step%i_LS_End'%i_step
+            lsStep['Value']['descr_short']='list files in working directory and sub-directory'
+            i_step += 1
---
+++ Interfaces/test/prod4_simtel_sst_launcher.py
@@ -0,0 +1,138 @@
+""" Launcher script to launch a Prod4Prod4SimtelSSTJob
+on the WMS or create a Transformation.
+
+    https://forge.in2p3.fr/issues/35179
+                    JB, October 2018
+"""
+
+import json
+from copy import copy
+
+from DIRAC.Core.Base import Script
+Script.setUsageMessage('\n'.join([__doc__.split('\n')[1],
+                                  'Usage:',
+                                  '  %s mode file_path (trans_name) group_size' % Script.scriptName,
+                                  'Arguments:',
+                                  '  mode: WMS for testing, TS for production',
+                                  '  trans_name: name of the transformation',
+                                  '  input_dataset_name: name of the input dataset',
+                                  '  group_size: n files to process',
+                                  '\ne.g: python %s.py WMS MyNewTrans Prod4-Paranal-gamma-North-DL-3 5' % Script.scriptName,
+                                 ]))
+
+Script.parseCommandLine()
+
+import DIRAC
+from DIRAC.TransformationSystem.Client.Transformation import Transformation
+from CTADIRAC.Interfaces.API.Prod4SimtelSSTJob import Prod4SimtelSSTJob
+from DIRAC.Core.Workflow.Parameter import Parameter
+from DIRAC.Interfaces.API.Dirac import Dirac
+from CTADIRAC.Core.Utilities.tool_box import get_dataset_MQ
+
+
+def submit_trans(job, trans_name, mqJson, group_size):
+    """ Create a transformation executing the job workflow
+    """
+    DIRAC.gLogger.notice('submit_trans : %s' % trans_name)
+
+    # Initialize JOB_ID
+    job.workflow.addParameter(Parameter("JOB_ID", "000000", "string", "", "",
+                                        True, False, "Temporary fix"))
+
+    trans = Transformation()
+    trans.setTransformationName(trans_name)  # this must be unique
+    trans.setType("DataReprocessing")
+    trans.setDescription("Prod4 Simtel TS")
+    trans.setLongDescription("Prod4 Simtel SST only processing")  # mandatory
+    trans.setBody(job.workflow.toXML())
+    trans.setGroupSize(group_size)
+    trans.setFileMask(mqJson) # catalog query is defined here
+    result = trans.addTransformation()  # transformation is created here
+    if not result['OK']:
+        return result
+    trans.setStatus("Active")
+    trans.setAgentType("Automatic")
+    trans_id = trans.getTransformationID()
+    return trans_id
+
+def submit_wms(job):
+    """ Submit the job to the WMS
+    @todo launch job locally
+    """
+    dirac = Dirac()
+    base_path = '/vo.cta.in2p3.fr/MC/PROD4/Paranal/proton/corsika/1829/Data/101xxx'
+    input_data = ['%s/run101000_proton_za20deg_azm0deg-paranal-sst.corsika.zst' % base_path,
+                  '%s/run101003_proton_za20deg_azm0deg-paranal-sst.corsika.zst' % base_path,
+                  '%s/run101006_proton_za20deg_azm0deg-paranal-sst.corsika.zst' % base_path]
+    job.setInputData(input_data)
+    job.setJobGroup('Prod4SimtelSSTJob')
+    result = dirac.submit(job)
+    if result['OK']:
+        Script.gLogger.notice('Submitted job: ', result['Value'])
+    return result
+
+def run_simtel_sst(args):
+    """ Simple wrapper to create a Prod4SimtelSSTJob and setup parameters
+        from positional arguments given on the command line.
+
+        Parameters:
+        args -- mode (trans_name dataset_name group_size)
+    """
+    DIRAC.gLogger.notice('run_simtel_sst')
+    # get arguments
+    mode = args[0]
+
+    if mode == 'TS':
+        trans_name = args[1]
+        dataset_name = args[2]
+        group_size = int(args[3])
+
+    # job setup
+    job = Prod4SimtelSSTJob()  # to be adjusted!!
+    # override for testing
+    job.setName('Prod4_Simtel')
+
+    # output
+    job.setOutputSandbox(['*Log.txt'])
+
+    # specific configuration
+    if mode == 'WMS':
+        job.base_path = '/vo.cta.in2p3.fr/user/b/bregeon'
+        job.ts_task_id = '123'
+        job.setupWorkflow(debug=True)
+        # subtmit to the WMS for debug
+        job.setDestination('LCG.IN2P3-CC.fr')
+        result = submit_wms(job)
+    elif mode == 'TS':
+        input_meta_query = get_dataset_MQ(dataset_name)
+        # refine output meta data if needed
+        output_meta_data = copy(input_meta_query)
+        job.set_meta_data(output_meta_data)
+
+        job.ts_task_id = '@{JOB_ID}'  # dynamic
+        job.setupWorkflow(debug=False)
+        job.setType('EvnDisp3')  # mandatory *here*
+        result = submit_trans(job, trans_name, json.dumps(input_meta_query), group_size)
+    else:
+        DIRAC.gLogger.error('1st argument should be the job mode: WMS or TS,\n\
+                             not %s' % mode)
+        return None
+
+    return result
+
+#########################################################
+if __name__ == '__main__':
+
+    arguments = Script.getPositionalArgs()
+    if len(arguments) not in [1, 4]:
+        Script.showHelp()
+    try:
+        result = run_simtel_sst(arguments)
+        if not result['OK']:
+            DIRAC.gLogger.error(result['Message'])
+            DIRAC.exit(-1)
+        else:
+            DIRAC.gLogger.notice('Done')
+    except Exception:
+        DIRAC.gLogger.exception()
+        DIRAC.exit(-1)
