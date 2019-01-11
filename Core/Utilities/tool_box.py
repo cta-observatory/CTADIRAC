c2ee04df1982c9f40fd8a361c766e34fd33a7816
BREGEON Johan <johan.bregeon@lupm.in2p3.fr>
2019-01-11 11:41:18 +0100

    fix run_number_from_filename for evendisplay

--- Core/Utilities/tool_box.py
+++ Core/Utilities/tool_box.py
@@ -46,6 +46,7 @@ def run_number_from_filename(filename, package):
     return:
         run_number : int - the run number
     """
+    run_number = -1
     if filename[-9:] == '.logs.tgz':
         run_number = int(filename.split('/')[-1].split('_')[-1].split('.')[0])
     elif package in ['chimp', 'mars']:
@@ -61,12 +62,12 @@ def run_number_from_filename(filename, package):
         else:
             run_number = int(filename.split('run')[1].split('___cta')[0])
     elif package == 'evndisplay':
-        if filename[-8:] in ['DL1.root', 'DL2.root']:
+        if filename.find('tid')>0:
+            run_number = int(re.findall(r'tid\d+', os.path.basename(filename))[0].strip('tid'))
+        elif filename[-8:] in ['DL1.root', 'DL2.root']:
             run_number = int(filename.split('run')[1].split('___cta')[0])
         elif filename[-10:] in ['DL1.tar.gz', 'DL2.tar.gz']:
             run_number = int(filename.split('run')[1].split('___cta')[0])
-        elif filename.find('tid')>0:
-            run_number = int(re.findall(r'tid\d+', os.path.basename(filename))[0].strip('tid'))
         else:
             run_number = int(filename.split('-')[0])  # old default
     elif package == 'image_extractor':
