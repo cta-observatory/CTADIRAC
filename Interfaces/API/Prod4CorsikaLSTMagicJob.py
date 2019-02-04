80940a74f2bf56d073be004fc3d637d216bab06f
Johan Bregeon <johan.bregeon@gmail.com>
2019-02-04 21:15:55 +0100

    LSTMagic production #35807

---
+++ Interfaces/API/Prod4CorsikaLSTMagicJob.py
@@ -0,0 +1,154 @@
+"""
+  Job wrapper class to handle Prod4 MC
+  Corsika simulations of the LST+MAGIC array
+			JB Februrary 2019
+"""
+
+__RCSID__ = "$Id$"
+
+# generic imports
+import json
+
+# DIRAC imports
+import DIRAC
+from CTADIRAC.Interfaces.API.Prod4CorsikaSSTJob import Prod4CorsikaSSTJob
+
+
+class Prod4CorsikaLSTMagicJob(Prod4CorsikaSSTJob):
+    """ Job extension class for Prod4 MC simulations,
+      takes care of running corsika for the LST+MAGIC positions
+      Inherits from Prod4CorsikaSSTJob
+      @todo should have a Prod4CorsikaJob really
+    """
+    def __init__(self, cpu_time=129600):
+        """ Constructor
+
+        Keyword arguments:
+        cpuTime -- max cpu time allowed for the job
+        """
+        Prod4CorsikaSSTJob.__init__(self)
+        self.setCPUTime(cpu_time)
+        self.version = '2018-11-07'
+        self.configuration_id = 5
+        self.cta_site = 'LaPalma'
+
+    def set_meta_data(self):
+        """ define the common meta data of the application
+        """
+        # The order of the metadata dictionary is important,
+        # since it's used to build the directory structure
+        self.metadata['array_layout'] = 'Baseline-LSTMagic'
+        self.metadata['site'] = self.cta_site
+        self.metadata['particle'] = self.particle
+        # air shower simulation means North=0 and South=180
+        if self.pointing_dir == 'North':
+            self.metadata['phiP'] = 0
+        if self.pointing_dir == 'South':
+            self.metadata['phiP'] = 180
+        self.metadata['thetaP'] = float(self.zenith_angle)
+        self.metadata[self.program_category + '_prog'] = self.prog_name
+        self.metadata[self.program_category + '_prog_version'] = self.version
+        self.metadata['data_level'] = self.output_data_level
+        self.metadata['configuration_id'] = self.configuration_id
+
+    def setupWorkflow(self, debug=False):
+        """ Setup job workflow by defining the sequence of all executables
+            All parameters shall have been defined before that method is called.
+        """
+        # Step 1 - debug only
+        i_step = 1
+
+        # Step 2 - setup software
+        sw_step = self.setExecutable('cta-prod3-setupsw',
+                                     arguments='%s %s' % (self.package, self.version),
+                                     logFile='SetupSoftware_Log.txt')
+        sw_step['Value']['name'] = 'Step%i_SetupSoftware' % i_step
+        sw_step['Value']['descr_short'] = 'Setup software'
+        i_step += 1
+
+        if debug:
+            ls_step = self.setExecutable('/bin/ls -alhtr', logFile='LS_Init_Log.txt')
+            ls_step['Value']['name'] = 'Step%i_LS_Init' % i_step
+            ls_step['Value']['descr_short'] = 'list files in working directory'
+            i_step += 1
+
+        # Step 3 - run corsika
+        if self.cta_site == 'LaPalma':
+            prod_script = './dirac_prod4_lst-magic_run'
+        else:
+            DIRAC.gLogger.error('Site not supported: %s' % self.cta_site)
+            DIRAC.gLogger.error('No shell script associated')
+            DIRAC.exit(-1)
+
+        cs_step = self.setExecutable(prod_script,
+                                     arguments='--without-multipipe --start_run %s \
+                                               --run %s %s %s %s %s' %
+                                     (self.start_run_number, self.run_number,
+                                      self.cta_site, self.particle,
+                                      self.pointing_dir, self.zenith_angle),
+                                     logFile='Corsika_Log.txt')
+        cs_step['Value']['name'] = 'Step%i_Corsika' % i_step
+        cs_step['Value']['descr_short'] = 'Run Corsika only'
+        i_step += 1
+
+        # Step 4 - verify size of corsika output
+        csv_step = self.setExecutable('cta-prod3-verifysteps',
+                                      arguments='generic %d %d %s' %
+                                      (self.n_output_files, self.output_file_size,
+                                       self.output_pattern),
+                                      logFile='Verify_Corsika_Log.txt')
+        csv_step['Value']['name'] = 'Step%i_VerifyCorsika' % i_step
+        csv_step['Value']['descr_short'] = 'Verify the Corsika run'
+        i_step += 1
+
+        # Step 5 - define meta data, upload file on SE and register in catalogs
+        self.set_meta_data()
+        md_json = json.dumps(self.metadata)
+
+        meta_data_field = {'array_layout': 'VARCHAR(128)', 'site': 'VARCHAR(128)',
+                           'particle': 'VARCHAR(128)',
+                           'phiP': 'float', 'thetaP': 'float',
+                           self.program_category + '_prog': 'VARCHAR(128)',
+                           self.program_category + '_prog_version': 'VARCHAR(128)',
+                           'data_level': 'int', 'configuration_id': 'int'}
+        md_field_json = json.dumps(meta_data_field)
+
+        # Upload and register data
+        file_meta_data = {}
+        file_md_json = json.dumps(file_meta_data)
+
+        scripts = '../CTADIRAC/Core/scripts/'
+        dm_step = self.setExecutable(scripts + 'cta-prod-managedata.py',
+                                     arguments="'%s' '%s' '%s' %s '%s' %s %s '%s' Data" %
+                                     (md_json, md_field_json, file_md_json,
+                                      self.base_path, self.output_pattern, self.package,
+                                      self.program_category, self.catalogs),
+                                     logFile='DataManagement_Log.txt')
+        dm_step['Value']['name'] = 'Step%s_DataManagement' % i_step
+        dm_step['Value']['descr_short'] = 'Save data files to SE and register them in DFC'
+        i_step += 1
+
+        # Upload and register log file
+        file_meta_data = {}
+        file_md_json = json.dumps(file_meta_data)
+        log_file_pattern = 'Data/corsika/run*/run*.log'
+        scripts = '../CTADIRAC/Core/scripts/'
+        log_step = self.setExecutable(scripts + 'cta-prod-managedata.py',
+                                      arguments="'%s' '%s' '%s' %s '%s' %s %s '%s' Log" %
+                                      (md_json, md_field_json, file_md_json,
+                                       self.base_path, log_file_pattern, self.package,
+                                       self.program_category, self.catalogs),
+                                      logFile='LogManagement_Log.txt')
+        log_step['Value']['name'] = 'Step%s_LogManagement' % i_step
+        log_step['Value']['descr_short'] = 'Save log to SE and register them in DFC'
+        i_step += 1
+
+        # Step 6 - debug only
+        if debug:
+            ls_step = self.setExecutable('/bin/ls -alhtr', logFile='LS_End_Log.txt')
+            ls_step['Value']['name'] = 'Step%s_LSHOME_End' % i_step
+            ls_step['Value']['descr_short'] = 'list files in Home directory'
+            i_step += 1
+
+        # Number of showers is passed via an environment variable
+        self.setExecutionEnv({'NSHOW': '%s' % self.n_shower})
---
+++ Interfaces/test/prod4_corsika_lst-magic_launcher.py
@@ -0,0 +1,132 @@
+""" Launcher script to launch a Prod4CorsikaLSTMagicJob
+on the WMS or create a Transformation.
+
+    https://forge.in2p3.fr/issues/3492
+                    JB, February 2019
+"""
+
+from DIRAC.Core.Base import Script
+Script.setUsageMessage('\n'.join([__doc__.split('\n')[1],
+                                  'Usage:',
+                                  '  %s mode file_path (trans_name) group_size' % Script.scriptName,
+                                  'Arguments:',
+                                  '  mode: WMS for testing, TS for production',
+                                  '  site: either Paranal or LaPalma',
+                                  '  particle: in gamma, gamma-diffuse, electron, proton',
+                                  '  pointing: North or South',
+                                  '  zenith: 20, 40 or 60',
+                                  '  n shower: 100 for testing',
+                                  '\ne.g: python %s.py WMS LaPalma gamma North 20 100' % Script.scriptName,
+                                 ]))
+
+Script.parseCommandLine()
+
+import DIRAC
+from DIRAC.TransformationSystem.Client.Transformation import Transformation
+from CTADIRAC.Interfaces.API.Prod4CorsikaLSTMagicJob import Prod4CorsikaLSTMagicJob
+from DIRAC.Core.Workflow.Parameter import Parameter
+from DIRAC.Interfaces.API.Dirac import Dirac
+
+
+def submit_trans(job, trans_name):
+    """ Create a transformation executing the job workflow
+    """
+    DIRAC.gLogger.notice('submit_trans : %s' % trans_name)
+
+    # Initialize JOB_ID
+    job.workflow.addParameter(Parameter("JOB_ID", "000000", "string", "", "",
+                                        True, False, "Temporary fix"))
+
+    trans = Transformation()
+    trans.setTransformationName(trans_name)  # this must be unique
+    trans.setType("MCSimulation")
+    trans.setDescription("Prod4 Corsika TS")
+    trans.setLongDescription("Prod4 Corsika LST-MAGIC simulation")  # mandatory
+    trans.setBody(job.workflow.toXML())
+    result = trans.addTransformation()  # transformation is created here
+    if not result['OK']:
+        return result
+    trans.setStatus("Active")
+    trans.setAgentType("Automatic")
+    trans_id = trans.getTransformationID()
+    return trans_id
+
+def submit_wms(job):
+    """ Submit the job to the WMS
+    @todo launch job locally
+    """
+    dirac = Dirac()
+    job.setJobGroup('Prod4CorsikaSSTJob')
+    result = dirac.submit(job)
+    if result['OK']:
+        Script.gLogger.notice('Submitted job: ', result['Value'])
+    return result
+
+def run_corsika_sst(args):
+    """ Simple wrapper to create a Prod4CorsikaLSTMagicJob and setup parameters
+        from positional arguments given on the command line.
+
+        Parameters:
+        args -- mode (trans_name)
+    """
+    DIRAC.gLogger.notice('run_corsika_sst')
+    # get arguments
+    mode = args[0]
+
+    # job setup
+    job = Prod4CorsikaLSTMagicJob()
+    # override for testing
+    job.setName('Prod4_Corsika_LSTMagic')
+    # parameters from command line
+    job.set_site(args[1])
+    job.set_particle(args[2])
+    job.set_pointing_dir(args[3])
+    job.zenith_angle = args[4]
+    job.n_shower = args[5]
+
+    # output
+    job.setOutputSandbox(['*Log.txt'])
+
+    # specific configuration
+    if mode == 'WMS':
+        job.base_path = '/vo.cta.in2p3.fr/user/b/bregeon'
+        job.start_run_number = '100'
+        job.run_number = '31'
+        job.setupWorkflow(debug=True)
+        # subtmit to the WMS for debug
+        job.setDestination('LCG.IN2P3-CC.fr')
+        # job.setDestination('LCG.CIEMAT.es')
+        result = submit_wms(job)
+    elif mode == 'TS':
+        job.base_path = '/vo.cta.in2p3.fr/MC/PRODTest/'
+        job.start_run_number = '100000'        
+        job.run_number = '@{JOB_ID}'  # dynamic
+        job.setupWorkflow(debug=False)
+        job.setType('MCSimulation')
+        tag = ''
+        trans_name = 'MC_Prod4_CorsikaLSTMagic_%s_%s_%s_%s%s' %\
+                    (job.cta_site, job.particle, job.pointing_dir, job.zenith_angle, tag)
+        result = submit_trans(job, trans_name)
+    else:
+        DIRAC.gLogger.error('1st argument should be the job mode: WMS or TS,\n\
+                             not %s' % mode)
+        return None
+
+    return result
+
+#########################################################
+if __name__ == '__main__':
+
+    arguments = Script.getPositionalArgs()
+    if len(arguments) != 6:
+        Script.showHelp()
+    try:
+        result = run_corsika_sst(arguments)
+        if not result['OK']:
+            DIRAC.gLogger.error(result['Message'])
+            DIRAC.exit(-1)
+        else:
+            DIRAC.gLogger.notice('Done')
+    except Exception:
+        DIRAC.gLogger.exception()
+        DIRAC.exit(-1)
