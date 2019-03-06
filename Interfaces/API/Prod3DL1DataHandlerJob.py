4bd1da14058969d68cd83d8ce927f4af5cddb109
Johan Bregeon <johan.bregeon@gmail.com>
2019-03-06 11:02:05 +0100

    job wrapper and launcher for dl1_data_handler #35992

---
+++ Interfaces/API/Prod3DL1DataHandlerJob.py
@@ -0,0 +1,156 @@
+"""
+  Wrapper on the Job class to handle dl1_data_handler DL0->"DL1"
+
+  https://forge.in2p3.fr/issues/35992
+                        March 6th 2019 - J. Bregeon
+  
+"""
+
+__RCSID__ = "$Id$"
+
+# generic imports
+import json, collections
+# DIRAC imports
+import DIRAC
+from DIRAC.Interfaces.API.Job import Job
+from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
+from CTADIRAC.Core.Utilities.tool_box import DATA_LEVEL_METADATA_ID
+
+
+class Prod3DL1DataHandlerJob(Job):
+    """ Job extension class for dl1_data_handler DL0->DL1 conversion
+      of simtel files to HDF5 conversion for Machine Learning fans
+    """
+
+    def __init__(self, cpuTime=36000):
+        """ Constructor
+
+        Keyword arguments:
+        cpuTime -- max cpu time allowed for the job
+        """
+        Job.__init__(self)
+        self.setCPUTime(cpuTime)
+        # defaults
+        self.setName('dl1 data handler reduction')
+        self.package = 'dl1_data_handler'
+        self.program_category = 'calibimgreco'
+        self.version = 'v0.7.2'
+        self.configuration_id = 1
+        self.output_data_level = DATA_LEVEL_METADATA_ID['DL1']
+        self.N_output_files = 1
+        self.prefix = 'CTA.prod3Sb'
+        self.layout = 'Baseline'
+        self.base_path = '/vo.cta.in2p3.fr/MC/PROD3/'
+        self.fcc = FileCatalogClient()
+        self.metadata = collections.OrderedDict()
+        self.catalogs = json.dumps(['DIRACFileCatalog', 'TSCatalog'])
+        self.ts_task_id = 0
+
+    def set_meta_data(self, simtel_md):
+        """ Set dl1_data_handler meta data 
+
+        Parameters:
+        simtel_md -- metadata dictionary as for simtel
+        """        
+        self.metadata['array_layout'] = simtel_md['array_layout']
+        self.metadata['site'] = simtel_md['site']
+        self.metadata['particle'] = simtel_md['particle']
+        try:
+            phiP = simtel_md['phiP']['=']
+        except:
+            phiP = simtel_md['phiP']
+        self.metadata['phiP'] = phiP
+        try:
+            thetaP = simtel_md['thetaP']['=']
+        except:
+            thetaP = simtel_md['thetaP']
+        self.metadata['thetaP'] = thetaP
+        self.metadata[self.program_category+'_prog'] = self.package
+        self.metadata[self.program_category+'_prog_version'] = self.version
+        self.metadata['data_level'] = self.output_data_level
+        self.metadata['configuration_id'] = self.configuration_id
+
+    def setupWorkflow(self, debug=False):
+        """ Setup job workflow by defining the sequence of all executables
+            All parameters shall have been defined before that method is called
+        """
+
+        # step 1 -- debug only
+        iStep = 1
+        if debug:
+            lsStep = self.setExecutable('/bin/ls -alhtr',
+                                        logFile='LS_Init_Log.txt')
+            lsStep['Value']['name'] = 'Step%i_LS_Init' % iStep
+            lsStep['Value']['descr_short'] = 'list files in working directory'
+            iStep += 1
+
+        # step 2
+        swStep = self.setExecutable('cta-prod3-setupsw',
+                                    arguments='%s %s' %
+                                    (self.package, self.version),
+                                    logFile='SetupSoftware_Log.txt')
+        swStep['Value']['name'] = 'Step%i_SetupSoftware' % iStep
+        swStep['Value']['descr_short'] = 'Setup software'
+        iStep += 1
+
+        # step 3
+        # arguments are nbFiles=0 (not used) and fileSize=100kB
+        eivStep = self.setExecutable('cta-prod3-verifysteps',
+                                     arguments='analysisinputs 0 100',
+                                     logFile='Verify_EvnDispInputs_Log.txt')
+        eivStep['Value']['name'] = 'Step%i_VerifyEvnDispInputs' % iStep
+        eivStep['Value']['descr_short'] = 'Verify EvnDisp Inputs'
+        iStep += 1
+
+        # step 4
+        evStep = self.setExecutable('./dirac_process_runs',
+                                    logFile='dl1_handler_Log.txt')
+        evStep['Value']['name'] = 'Step%i_dl1_data_handler' % iStep
+        evStep['Value']['descr_short'] = 'Run the DL1 Data Handler'
+        iStep += 1
+
+        # Step 5 - define meta data, upload file on SE and register in catalogs
+        md_json = json.dumps(self.metadata)
+        meta_data_field = {'array_layout': 'VARCHAR(128)', 'site': 'VARCHAR(128)',
+                           'particle': 'VARCHAR(128)',
+                           'phiP': 'float', 'thetaP': 'float',
+                           self.program_category + '_prog': 'VARCHAR(128)',
+                           self.program_category + '_prog_version': 'VARCHAR(128)',
+                           'data_level': 'int', 'configuration_id': 'int'}
+        md_field_json = json.dumps(meta_data_field)
+
+        # register Data
+        file_meta_data = {}
+        file_md_json = json.dumps(file_meta_data)
+        output_pattern = './Data/*.h5'
+        scripts = '../CTADIRAC/Core/scripts'
+        dmStep = self.setExecutable(scripts + '/cta-analysis-managedata.py',
+                            arguments = "'%s' '%s' '%s' %s '%s' %s %s '%s'" %\
+                            (md_json, md_field_json, file_md_json, self.base_path,
+                            output_pattern, self.package, self.program_category,
+                            self.catalogs),
+                            logFile='DataManagement_Log.txt')
+        dmStep['Value']['name'] = 'Step%i_DataManagement' % iStep
+        dmStep['Value']['descr_short'] = 'Save data files to SE and register them in DFC'
+        iStep += 1
+
+        # register Log
+        # outputpattern = './*.logs.tgz'
+        # filemetadata = {}
+        # file_md_json = json.dumps(filemetadata)
+        # dmStep = self.setExecutable('../CTADIRAC/Core/scripts/cta-analysis-managedata.py',
+        #                           arguments = "'%s' '%s' '%s' %s '%s' %s %s '%s' Log" % \
+        #                           (mdjson, mdfieldjson, file_md_json, self.basepath,
+        #                            outputpattern, self.package, self.program_category, self.catalogs),
+        #                           logFile = 'Log_DataManagement_Log.txt')
+        # dmStep['Value']['name'] = 'Step%i_Log_DataManagement' % iStep
+        # dmStep['Value']['descr_short'] = 'Save log files to SE and register them in DFC'
+        # iStep += 1
+
+        # step 6 -- to be removed -- debug only
+        if debug:
+            lsStep = self.setExecutable('/bin/ls -alhtr ./ ./Data',
+                                        logFile='LS_End_Log.txt')
+            lsStep['Value']['name'] = 'Step%i_LS_End' % iStep
+            lsStep['Value']['descr_short'] = 'list files in working and Data directory'
+            iStep += 1
---
+++ Interfaces/test/prod3_dl1_data_handler_launcher.py
@@ -0,0 +1,139 @@
+""" Launcher script script to launch a DL1 Data Handler conversion
+on the WMS or create a Transformation.
+
+  https://forge.in2p3.fr/issues/35992
+                        March 6th 2019 - J. Bregeon
+"""
+
+import json
+from copy import copy
+
+from DIRAC.Core.Base import Script
+Script.setUsageMessage('\n'.join([__doc__.split('\n')[1],
+                                  'Usage:',
+                                  '  %s mode file_path (trans_name) group_size' % Script.scriptName,
+                                  'Arguments:',
+                                  '  mode: WMS for testing, TS for production',
+                                  '  trans_name: name of the transformation',
+                                  '  input_dataset_name: name of the input dataset',
+                                  '  group_size: n files to process',
+                                  '\ne.g: python %s.py WMS MyNewTrans Prod3_LaPalma_Baseline_NSB1x_gamma_North_20deg_DL0 5' % Script.scriptName,
+                                 ]))
+
+Script.parseCommandLine()
+
+import DIRAC
+from DIRAC.TransformationSystem.Client.Transformation import Transformation
+from CTADIRAC.Interfaces.API.Prod3DL1DataHandlerJob import Prod3DL1DataHandlerJob
+from DIRAC.Core.Workflow.Parameter import Parameter
+from DIRAC.Interfaces.API.Dirac import Dirac
+from CTADIRAC.Core.Utilities.tool_box import get_dataset_MQ
+
+
+def submit_trans(job, trans_name, mqJson, group_size):
+    """ Create a transformation executing the job workflow
+    """
+    DIRAC.gLogger.notice('submit_trans : %s' % trans_name)
+
+    # Initialize JOB_ID
+    job.workflow.addParameter(Parameter("JOB_ID", "000000", "string", "", "",
+                                        True, False, "Temporary fix"))
+
+    trans = Transformation()
+    trans.setTransformationName(trans_name)  # this must be unique
+    trans.setType("DataReprocessing")
+    trans.setDescription("Prod3 DL1 Data Handler TS")
+    trans.setLongDescription("Prod3 DL1 Data Handler conversion")  # mandatory
+    trans.setBody(job.workflow.toXML())
+    trans.setGroupSize(group_size)
+    trans.setFileMask(mqJson) # catalog query is defined here
+    result = trans.addTransformation()  # transformation is created here
+    if not result['OK']:
+        return result
+    trans.setStatus("Active")
+    trans.setAgentType("Automatic")
+    trans_id = trans.getTransformationID()
+    return trans_id
+
+def submit_wms(job):
+    """ Submit the job to the WMS
+    @todo launch job locally
+    """
+    dirac = Dirac()
+    base_path = '/vo.cta.in2p3.fr/MC/PROD3/LaPalma/proton/simtel/1602/Data/000xxx/'
+    input_data = ['%s/proton_20deg_0deg_run100___cta-prod3-demo-2147m-LaPalma-baseline.simtel.gz' % base_path]
+
+    job.setInputData(input_data)
+    job.setJobGroup('DL1DataHandlerJob')
+    result = dirac.submit(job)
+    if result['OK']:
+        Script.gLogger.notice('Submitted job: ', result['Value'])
+    return result
+
+def launch_job(args):
+    """ Simple launcher to instanciate a Job and setup parameters
+        from positional arguments given on the command line.
+
+        Parameters:
+        args -- mode (trans_name dataset_name group_size)
+    """
+    DIRAC.gLogger.notice('run_evndisp_sst')
+    # get arguments
+    mode = args[0]
+
+    if mode == 'TS':
+        trans_name = args[1]
+        dataset_name = args[2]
+        group_size = int(args[3])
+
+    # job setup - 72 hours
+    job = Prod3DL1DataHandlerJob(cpuTime=259200.)
+    # override for testing
+    job.setName('Prod3_DL1DataHandler')
+    # output
+    job.setOutputSandbox(['*Log.txt'])
+
+    # specific configuration
+    if mode == 'WMS':
+        job.base_path = '/vo.cta.in2p3.fr/user/b/bregeon'
+        job.ts_task_id = '123'
+        simtel_meta_data = {'array_layout': 'Baseline', 'site': 'LaPalma',
+                           'particle': 'proton', 'phiP': 180.0, 'thetaP': 20.0,
+                           'nsb': 1}
+        job.set_meta_data(simtel_meta_data)
+        job.setupWorkflow(debug=True)
+        # subtmit to the WMS for debug
+        job.setDestination('LCG.IN2P3-CC.fr')
+        result = submit_wms(job)
+    elif mode == 'TS':
+        input_meta_query = get_dataset_MQ(dataset_name)
+        # refine output meta data if needed
+        output_meta_data = copy(input_meta_query)
+        job.set_meta_data(output_meta_data)
+        job.ts_task_id = '@{JOB_ID}'  # dynamic
+        job.setupWorkflow(debug=False)
+        job.setType('EvnDisp3')  # mandatory *here*
+        result = submit_trans(job, trans_name, json.dumps(input_meta_query), group_size)
+    else:
+        DIRAC.gLogger.error('1st argument should be the job mode: WMS or TS,\n\
+                             not %s' % mode)
+        return None
+
+    return result
+
+#########################################################
+if __name__ == '__main__':
+
+    arguments = Script.getPositionalArgs()
+    if len(arguments) not in [1, 4]:
+        Script.showHelp()
+    try:
+        result = launch_job(arguments)
+        if not result['OK']:
+            DIRAC.gLogger.error(result['Message'])
+            DIRAC.exit(-1)
+        else:
+            DIRAC.gLogger.notice('Done')
+    except Exception:
+        DIRAC.gLogger.exception()
+        DIRAC.exit(-1)
