776bd58b51c19503ad06bbd425292bcd262e7ec0
bregeon <johan.bregeon@gmail.com>
2020-11-23 18:38:23 +0100

    New API to run EventDisplay container on Prod5

---
+++ Interfaces/API/EvnDispProd5SingJob.py
@@ -0,0 +1,164 @@
+"""
+  Simple Wrapper on the Job class to handle EvnDisp Analysis
+  for the Prod5b 2020 analysis from container
+  https://forge.in2p3.fr/issues/42322
+
+  November 23th 2020 - J. Bregeon
+                  bregeon@in2p3.fr
+"""
+
+__RCSID__ = "$Id$"
+
+# generic imports
+import json
+import collections
+# DIRAC imports
+import DIRAC
+from DIRAC.Interfaces.API.Job import Job
+
+
+class EvnDispProd5SingJob(Job):
+    """ Job extension class for EvnDisp Analysis,
+      takes care of running converter and evndisp.
+    """
+
+    def __init__(self, cpuTime=432000):
+        """ Constructor
+
+        Keyword arguments:
+        cpuTime -- max cpu time allowed for the job
+        """
+        Job.__init__(self)
+        self.setCPUTime(cpuTime)
+        # defaults
+        self.setName('Evndisplay_CalibReco')
+        self.package = 'evndisplay'
+        self.version = 'eventdisplay-cta-dl1-prod5.v02'
+        self.compiler='gcc48_default'
+        self.container = True
+        self.program_category = 'calibimgreco'
+        self.prog_name = 'evndisp'
+        self.configuration_id = 8
+        self.output_data_level = 1
+        self.base_path = '/vo.cta.in2p3.fr/MC/PROD5b/'
+        self.metadata = collections.OrderedDict()
+        self.file_meta_data = dict()
+        self.catalogs = json.dumps(['DIRACFileCatalog', 'TSCatalog'])
+        self.ts_task_id = 0
+
+    def set_meta_data(self, tel_sim_md):
+        """ Set EventDisplay meta data
+
+        Parameters:
+        tel_sim_md -- metadata dictionary from the telescope simulation
+        """
+        # # Set evndisp directory meta data
+        self.metadata['array_layout'] = tel_sim_md['array_layout']
+        self.metadata['site'] = tel_sim_md['site']
+        self.metadata['particle'] = tel_sim_md['particle']
+        try:
+            phiP = tel_sim_md['phiP']['=']
+        except TypeError:
+            phiP = tel_sim_md['phiP']
+        self.metadata['phiP'] = phiP
+        try:
+            thetaP = tel_sim_md['thetaP']['=']
+        except TypeError:
+            thetaP = tel_sim_md['thetaP']
+        self.metadata['thetaP'] = thetaP
+        self.metadata[self.program_category+'_prog'] = self.prog_name
+        self.metadata[self.program_category+'_prog_version'] = self.version
+        self.metadata['data_level'] = self.output_data_level
+        self.metadata['configuration_id'] = self.configuration_id
+
+    def set_file_meta_data(self, nsb=1):
+        """ Set evndisplay file meta data
+
+        Parameters:
+        meta_data_dict -- metadata dictionary
+        """
+        # Set evndisp file meta data
+        self.file_meta_data['nsb'] = nsb
+
+    def setupWorkflow(self, debug=False):
+        """ Setup job workflow by defining the sequence of all executables
+            All parameters shall have been defined before that method is called.
+        """
+        i_step = 0
+        # step 1 -- debug
+        if debug:
+            ls_step = self.setExecutable('/bin/ls -alhtr', logFile='LS_Init_Log.txt')
+            ls_step['Value']['name'] = 'Step%i_LS_Init' % i_step
+            ls_step['Value']['descr_short'] = 'list files in working directory'
+            i_step += 1
+
+            env_step = self.setExecutable('/bin/env', logFile='Env_Log.txt')
+            env_step['Value']['name'] = 'Step%i_Env' % i_step
+            env_step['Value']['descr_short'] = 'Dump environment'
+            i_step += 1
+
+        # step 2
+        sw_step = self.setExecutable('cta-prod-setup-software',
+                                  arguments='-p %s -v %s -a simulations -g %s'%
+                                  (self.package, self.version, self.compiler),\
+                                  logFile='SetupSoftware_Log.txt')
+        sw_step['Value']['name'] = 'Step%i_SetupSoftware' % i_step
+        sw_step['Value']['descr_short'] = 'Setup software'
+        i_step+=1
+
+        # step 3 verify input data size
+        # arguments are nbFiles=0 (not used) and fileSize=1000kB
+        eiv_step = self.setExecutable('cta-prod3-verifysteps',
+                            arguments="generic 1 1000 '*.simtel.zst'",
+                            logFile='Verify_EvnDispInputs_Log.txt')
+        eiv_step['Value']['name'] = 'Step%i_VerifyEvnDispInputs' % i_step
+        eiv_step['Value']['descr_short'] = 'Verify EvnDisp Inputs'
+        i_step += 1
+
+        # step 4 run EventDisplay
+        ev_step = self.setExecutable('./dirac_sing_evndisp.sh',
+                                     logFile='EvnDisp_Log.txt')
+        ev_step['Value']['name'] = 'Step%i_EvnDisplay' % i_step
+        ev_step['Value']['descr_short'] = 'Run EvnDisplay'
+        i_step += 1
+
+        # step 5 set meta data and register Data
+        meta_data_json = json.dumps(self.metadata)
+        file_meta_data_json = json.dumps(self.file_meta_data)
+
+        meta_data_field = {'array_layout': 'VARCHAR(128)', 'site': 'VARCHAR(128)',
+                           'particle': 'VARCHAR(128)',
+                           'phiP': 'float', 'thetaP': 'float',
+                           self.program_category+'_prog': 'VARCHAR(128)',
+		                   self.program_category+'_prog_version': 'VARCHAR(128)',
+                           'data_level': 'int', 'configuration_id': 'int'}
+        meta_data_field_json = json.dumps(meta_data_field)
+
+        # register Data
+        data_output_pattern = './Data/*.DL1.root'
+        scripts = '../CTADIRAC/Core/scripts/'
+        dm_step = self.setExecutable(scripts + 'cta-prod-managedata.py',
+                                     arguments="'%s' '%s' '%s' %s '%s' %s %s '%s' Data" %
+                                     (meta_data_json, meta_data_field_json,
+                                      file_meta_data_json,
+                                      self.base_path, data_output_pattern, self.package,
+                                      self.program_category, self.catalogs),
+                                     logFile='DataManagement_Log.txt')
+        dm_step['Value']['name'] = 'Step%s_DataManagement' % i_step
+        dm_step['Value']['descr_short'] = 'Save data files to SE and register them in DFC'
+        i_step += 1
+
+        # step 7 failover step
+        if not debug:
+            failover_step = self.setExecutable('/bin/ls -l',
+                                               modulesList=['Script', 'FailoverRequest'])
+            failover_step['Value']['name'] = 'Step%s_Failover' % i_step
+            failover_step['Value']['descr_short'] = 'Tag files as unused if job failed'
+            i_step += 1
+
+        # Step 8 - debug only
+        if debug:
+            ls_step = self.setExecutable('/bin/ls -Ralhtr', logFile='LS_End_Log.txt')
+            ls_step['Value']['name'] = 'Step%s_LSHOME_End' % i_step
+            ls_step['Value']['descr_short'] = 'list files in Home directory'
+            i_step += 1
---
+++ Interfaces/test/prod5b_sing_evndisp_launcher.py
@@ -0,0 +1,145 @@
+""" Launcher script to launch a Prod3 EvnDispProd5SingJob
+on the WMS or create a Transformation.
+
+    https://forge.in2p3.fr/issues/441709
+
+   November 16th 2020 - J. Bregeon
+                  bregeon@in2p3.fr
+"""
+
+from copy import copy
+
+from DIRAC.Core.Base import Script
+Script.setUsageMessage('\n'.join([__doc__.split('\n')[1],
+                                  'Usage:',
+                                  '  %s mode file_path (trans_name) group_size' % Script.scriptName,
+                                  'Arguments:',
+                                  '  mode: WMS for testing, TS for production',
+                                  '  trans_name: name of the transformation',
+                                  '  input_dataset_name: name of the input dataset',
+                                  '  group_size: n files to process',
+                                  '\ne.g: python %s.py WMS MyNewTrans Prod4-Paranal-gamma-North-DL-3 5' % Script.scriptName,
+                                 ]))
+
+Script.parseCommandLine()
+
+import DIRAC
+from DIRAC.TransformationSystem.Client.Transformation import Transformation
+from CTADIRAC.Interfaces.API.EvnDispProd5SingJob import EvnDispProd5SingJob
+from DIRAC.Core.Workflow.Parameter import Parameter
+from DIRAC.Interfaces.API.Dirac import Dirac
+from CTADIRAC.Core.Utilities.tool_box import get_dataset_MQ
+
+
+def submit_trans(job, trans_name, input_meta_query, group_size):
+    """ Create a transformation executing the job workflow
+    """
+    DIRAC.gLogger.notice('submit_trans : %s' % trans_name)
+
+    # Initialize JOB_ID
+    job.workflow.addParameter(Parameter("JOB_ID", "000000", "string", "", "",
+                                        True, False, "Temporary fix"))
+
+    trans = Transformation()
+    trans.setTransformationName(trans_name)  # this must be unique
+    trans.setType("DataReprocessing")
+    trans.setDescription("Prod5 EventDisplay TS")
+    trans.setLongDescription("Prod5 EventDisplay processing")  # mandatory
+    trans.setBody(job.workflow.toXML())
+    trans.setGroupSize(group_size)
+    trans.setInputMetaQuery(input_meta_query)
+    result = trans.addTransformation()  # transformation is created here
+    if not result['OK']:
+        return result
+    trans.setStatus("Active")
+    trans.setAgentType("Automatic")
+    trans_id = trans.getTransformationID()
+    return trans_id
+
+
+def submit_wms(job):
+    """ Submit the job to the WMS
+    @todo launch job locally
+    """
+    dirac = Dirac()
+    base_path = '/vo.cta.in2p3.fr/MC/PROD5b/LaPalma/proton/sim_telarray/2295/Data/000xxx'
+    input_data = ['%s/proton_20deg_0deg_run744___cta-prod5b-lapalma_desert-2158m-LaPalma-dark.simtel.zst' % base_path]  # CC-IN2P3
+
+    job.setInputData(input_data)
+    job.setJobGroup('EvnDispProd5')
+    result = dirac.submitJob(job)
+    if result['OK']:
+        Script.gLogger.notice('Submitted job: ', result['Value'])
+    return result
+
+
+def launch_job(args):
+    """ Simple launcher to instanciate a Job and setup parameters
+        from positional arguments given on the command line.
+
+        Parameters:
+        args -- mode (trans_name dataset_name group_size)
+    """
+    DIRAC.gLogger.notice('Running EventDisplay jobs')
+    # get arguments
+    mode = args[0]
+
+    if mode == 'TS':
+        trans_name = args[1]
+        dataset_name = args[2]
+        group_size = int(args[3])
+
+    # job setup - 72 hours
+    job = EvnDispProd5SingJob(cpuTime=259200.)
+    # override for testing
+    job.setName('Prod5b_EvnDisp')
+    # output
+    job.setOutputSandbox(['*Log.txt'])
+
+    # specific configuration
+    if mode == 'WMS':
+        job.base_path = '/vo.cta.in2p3.fr/user/b/bregeon'
+        job.ts_task_id = '62'
+        simtel_meta_data = {'array_layout': 'Advanced-Baseline', 'site': 'LaPalma',
+                           'particle': 'proton', 'phiP': 0.0, 'thetaP': 20.0}
+        job.set_meta_data(simtel_meta_data)
+        job.set_file_meta_data(nsb=1)
+        job.setupWorkflow(debug=True)
+        # subtmit to the WMS for debug
+        # job.setDestination('LCG.IN2P3-CC.fr')
+        result = submit_wms(job)
+    elif mode == 'TS':
+        input_meta_query = get_dataset_MQ(dataset_name)
+        # refine output meta data if needed
+        output_meta_data = copy(input_meta_query)
+        job.set_meta_data(output_meta_data)
+        job.set_file_meta_data(nsb=output_meta_data['nsb']['='])
+        # adjust calibration file
+        job.ts_task_id = '@{JOB_ID}'  # dynamic
+        job.setupWorkflow(debug=False)
+        job.setType('EvnDisp3')  # mandatory *here*
+        result = submit_trans(job, trans_name, input_meta_query, group_size)
+    else:
+        DIRAC.gLogger.error('1st argument should be the job mode: WMS or TS,\n\
+                             not %s' % mode)
+        return None
+
+    return result
+
+
+#########################################################
+if __name__ == '__main__':
+
+    arguments = Script.getPositionalArgs()
+    if len(arguments) not in [1, 4]:
+        Script.showHelp()
+    try:
+        result = launch_job(arguments)
+        if not result['OK']:
+            DIRAC.gLogger.error(result['Message'])
+            DIRAC.exit(-1)
+        else:
+            DIRAC.gLogger.notice('Done')
+    except Exception:
+        DIRAC.gLogger.exception()
+        DIRAC.exit(-1)
